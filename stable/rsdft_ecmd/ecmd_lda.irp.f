75b77711660cb540ebd2c226d7213aabe1bc2afa
Emmanuel Giner <giner.emmanuel@gmail.com>
2018-12-28 15:35:09 +0100

    renamed some stuffs

--- stable/rsdft_ecmd/ecmd_lda.irp.f
+++ stable/rsdft_ecmd/ecmd_lda.irp.f
@@ -3,10 +3,10 @@
  BEGIN_DOC
  ! Corelation energy for the multi determinent short range LDA. PRB 73 155111 2006
  END_DOC
- integer :: j,k,l,istate 
+ integer :: ipoint,istate 
  double precision, allocatable :: aos_array(:), r(:), rho_a(:), rho_b(:), ec(:)
  logical :: dospin
- double precision :: r2(3),dr2(3), local_potential,r12,dx2,mu,coulomb,two_body_dm
+ double precision :: r2(3),dr2(3), local_potential,r12,dx2,mu,coulomb,two_body_dm,weight
  double precision :: threshold
  double precision :: cpu0,cpu1
  dospin = .True. ! JT dospin have to be set to true for open shell
@@ -14,22 +14,17 @@
  Energy_c_md_LDA = 0.d0
  allocate(aos_array(ao_num),r(3), rho_a(N_states), rho_b(N_states), ec(N_states))
  call cpu_time(cpu0)
-  do j = 1, nucl_num
-   do k = 1, n_points_radial_grid  -1
-    do l = 1, n_points_integration_angular 
-     
-     r(1) = grid_points_per_atom(1,l,k,j)
-     r(2) = grid_points_per_atom(2,l,k,j)
-     r(3) = grid_points_per_atom(3,l,k,j)
-     call dm_dft_alpha_beta_and_all_aos_at_r(r,rho_a,rho_b,aos_array)
-     if(dabs(final_weight_at_r_vector(l,k,j) * (rho_a(1)+rho_b(1))).lt.threshold)cycle
-
-     do istate = 1, N_states
-!!!!!!!!!!!! CORRELATION PART
-      call ESRC_MD_LDAERF (mu_erf_dft,rho_a(istate),rho_b(istate),dospin,ec(istate))
-      Energy_c_md_LDA(istate) += final_weight_at_r_vector(l,k,j) * ec(istate)
-     enddo
-    enddo
+  do ipoint = 1, n_points_final_grid
+   r(1) = final_grid_points(1,ipoint)
+   r(2) = final_grid_points(2,ipoint)
+   r(3) = final_grid_points(3,ipoint)
+   call dm_dft_alpha_beta_and_all_aos_at_r(r,rho_a,rho_b,aos_array)
+   weight = final_weight_at_r_vector(ipoint)
+   do istate = 1, N_states
+    if(dabs(weight * (rho_a(istate)+rho_b(istate))).lt.threshold)cycle
+!!!!!!!!!! CORRELATION PART
+    call ESRC_MD_LDAERF (mu_erf_dft,rho_a(istate),rho_b(istate),dospin,ec(istate))
+    Energy_c_md_LDA(istate) += weight * ec(istate)
    enddo
   enddo
  deallocate(aos_array,r,rho_a,rho_b, ec)
--- stable/two_body_dm/providers_two_dm_approx_svd.irp.f
+++ stable/two_body_dm/providers_two_dm_approx_svd.irp.f
@@ -18,10 +18,10 @@
 
 END_PROVIDER
 
- BEGIN_PROVIDER [integer, n_singular_approx_svd_two_dm, (N_states)]
-&BEGIN_PROVIDER [integer, n_max_singular_approx_svd_two_dm]
+ BEGIN_PROVIDER [integer, n_approx_svd_two_dm, (N_states)]
+&BEGIN_PROVIDER [integer, n_max_approx_svd_two_dm]
  BEGIN_DOC 
-! Number of singular vectors (left and right) kept to represent the two-body tensor of each states
+! Number of svd vectors (l and r) kept to represent the two-body tensor of each states
 ! This number depends on thr_ontop_approx 
  END_DOC
  implicit none
@@ -67,30 +67,30 @@ END_PROVIDER
   call wall_time(wall_2)
  print*,'*****SVD is done ******' 
  print*,'Wall time for SVD = ',wall_2 - wall_1
-  n_singular_approx_svd_two_dm(istate) =1 
-  print*,n_singular_approx_svd_two_dm(istate),D_i(n_singular_approx_svd_two_dm(istate))
-  do while ( (dabs(D_i(n_singular_approx_svd_two_dm(istate))) .gt. thresh) .AND. (n_singular_approx_svd_two_dm(istate) .lt. mo_tot_num**2)  )
-   n_singular_approx_svd_two_dm(istate) += 1
-   print*,n_singular_approx_svd_two_dm(istate),D_i(n_singular_approx_svd_two_dm(istate))
+  n_approx_svd_two_dm(istate) =1 
+  print*,n_approx_svd_two_dm(istate),D_i(n_approx_svd_two_dm(istate))
+  do while ( (dabs(D_i(n_approx_svd_two_dm(istate))) .gt. thresh) .AND. (n_approx_svd_two_dm(istate) .lt. mo_tot_num**2)  )
+   n_approx_svd_two_dm(istate) += 1
+   print*,n_approx_svd_two_dm(istate),D_i(n_approx_svd_two_dm(istate))
   enddo
-  if (dabs(D_i(n_singular_approx_svd_two_dm(istate))) .lt. thresh) then
-   n_singular_approx_svd_two_dm(istate) -= 1
+  if (dabs(D_i(n_approx_svd_two_dm(istate))) .lt. thresh) then
+   n_approx_svd_two_dm(istate) -= 1
   endif
   print*,'************************'
-  print*,'n_eigen_approx_two_dm  =',n_singular_approx_svd_two_dm(istate)
+  print*,'n_eigen_approx_two_dm  =',n_approx_svd_two_dm(istate)
   print*,'************************'
  enddo
- n_max_singular_approx_svd_two_dm = maxval(n_singular_approx_svd_two_dm)
+ n_max_approx_svd_two_dm = maxval(n_approx_svd_two_dm)
 
 END_PROVIDER
 
- BEGIN_PROVIDER [double precision, singular_approx_svd_two_dm, (n_max_singular_approx_svd_two_dm, N_states)]
-&BEGIN_PROVIDER [double precision, singular_left_vec_approx_svd_two_dm, (mo_tot_num**2,n_max_singular_approx_svd_two_dm,N_states)]
-&BEGIN_PROVIDER [double precision, singular_right_vec_approx_svd_two_dm, (mo_tot_num**2,n_max_singular_approx_svd_two_dm,N_states)]
+ BEGIN_PROVIDER [double precision, approx_svd_two_dm, (n_max_approx_svd_two_dm, N_states)]
+&BEGIN_PROVIDER [double precision, l_vec_approx_svd_two_dm, (mo_tot_num**2,n_max_approx_svd_two_dm,N_states)]
+&BEGIN_PROVIDER [double precision, r_vec_approx_svd_two_dm, (mo_tot_num**2,n_max_approx_svd_two_dm,N_states)]
  BEGIN_DOC 
-! singular_approx_svd_two_dm(i,istate) = ith Singular values to represent the two-body tensor of the state istate 
-! singular_left_vec_approx_svd_two_dm(I,i,istate) = component on the couple of MOs approx_two_dm_map(I,1) and approx_two_dm_map(I,2) of the ith Singular left vector of the state istate 
-! singular_right_vec_approx_svd_two_dm(I,i,istate) = component on the couple of MOs approx_two_dm_map(I,1) and approx_two_dm_map(I,2) of the ith Singular right vector of the state istate 
+! approx_svd_two_dm(i,istate) = ith Singular values to represent the two-body tensor of the state istate 
+! l_vec_approx_svd_two_dm(I,i,istate) = component on the couple of MOs approx_two_dm_map(I,1) and approx_two_dm_map(I,2) of the ith Singular l vector of the state istate 
+! r_vec_approx_svd_two_dm(I,i,istate) = component on the couple of MOs approx_two_dm_map(I,1) and approx_two_dm_map(I,2) of the ith Singular r vector of the state istate 
  END_DOC
  implicit none
  integer :: i,j,k,l,ij,kl
@@ -121,11 +121,11 @@ END_PROVIDER
  
    call svd(mat_i,size(mat_i,1),u_i,size(u_i,1),D_i,vt_i,size(vt_i,1),size(mat_i,1),size(mat_i,2))
  
-  do i = 1,n_singular_approx_svd_two_dm(istate)
-   singular_approx_svd_two_dm(i,istate) = D_i(i)
+  do i = 1,n_approx_svd_two_dm(istate)
+   approx_svd_two_dm(i,istate) = D_i(i)
    do j = 1,mo_tot_num**2
-    singular_left_vec_approx_svd_two_dm(j,i,istate) = u_i(j,i)
-    singular_right_vec_approx_svd_two_dm(j,i,istate) = vt_i(i,j)
+    l_vec_approx_svd_two_dm(j,i,istate) = u_i(j,i)
+    r_vec_approx_svd_two_dm(j,i,istate) = vt_i(i,j)
    enddo 
   enddo
  enddo
@@ -133,25 +133,25 @@ END_PROVIDER
 END_PROVIDER
 
 
- BEGIN_PROVIDER [double precision, singular_left_vec_approx_svd_two_dm_at_r , (n_max_singular_approx_svd_two_dm,n_points_final_grid,N_states)]
-&BEGIN_PROVIDER [double precision, singular_right_vec_approx_svd_two_dm_at_r, (n_max_singular_approx_svd_two_dm,n_points_final_grid,N_states)]
+ BEGIN_PROVIDER [double precision, l_vec_approx_svd_two_dm_at_r , (n_max_approx_svd_two_dm,n_points_final_grid,N_states)]
+&BEGIN_PROVIDER [double precision, r_vec_approx_svd_two_dm_at_r, (n_max_approx_svd_two_dm,n_points_final_grid,N_states)]
  BEGIN_DOC 
-! singular_left_vec_approx_svd_two_dm_at_r(i,j,istate) = value in real space of the ith left singular vector at the jth point of the grid for the state istate
-! singular_right_vec_approx_svd_two_dm_at_r(i,j,istate) = value in real space of the ith right singular vector at the jth point of the grid for the state istate
+! l_vec_approx_svd_two_dm_at_r(i,j,istate) = value in real space of the ith l svd vector at the jth point of the grid for the state istate
+! r_vec_approx_svd_two_dm_at_r(i,j,istate) = value in real space of the ith r svd vector at the jth point of the grid for the state istate
  END_DOC
  implicit none
  integer :: i,j,k
 
- singular_left_vec_approx_svd_two_dm_at_r = 0d0
- singular_right_vec_approx_svd_two_dm_at_r = 0d0
+ l_vec_approx_svd_two_dm_at_r = 0d0
+ r_vec_approx_svd_two_dm_at_r = 0d0
  !!!!!!unfoldage!!!!!!!
  integer :: istate
  do istate = 1, N_states
   do i = 1,n_points_final_grid
-   do j = 1,n_singular_approx_svd_two_dm(istate) 
+   do j = 1,n_approx_svd_two_dm(istate) 
     do k = 1,mo_tot_num**2
-     singular_left_vec_approx_svd_two_dm_at_r(j,i,istate) +=  singular_left_vec_approx_svd_two_dm(k,j,istate) * mos_in_r_array(approx_two_dm_map(k,1),i) * mos_in_r_array(approx_two_dm_map(k,2),i)
-     singular_right_vec_approx_svd_two_dm_at_r(j,i,istate)+= singular_right_vec_approx_svd_two_dm(k,j,istate) * mos_in_r_array(approx_two_dm_map(k,1),i) * mos_in_r_array(approx_two_dm_map(k,2),i) 
+     l_vec_approx_svd_two_dm_at_r(j,i,istate) +=  l_vec_approx_svd_two_dm(k,j,istate) * mos_in_r_array(approx_two_dm_map(k,1),i) * mos_in_r_array(approx_two_dm_map(k,2),i)
+     r_vec_approx_svd_two_dm_at_r(j,i,istate)+= r_vec_approx_svd_two_dm(k,j,istate) * mos_in_r_array(approx_two_dm_map(k,1),i) * mos_in_r_array(approx_two_dm_map(k,2),i) 
     enddo
    enddo
   enddo
@@ -159,7 +159,7 @@ END_PROVIDER
 
 END_PROVIDER
 
-BEGIN_PROVIDER [double precision, integral_on_top_of_r_approx_svd, (N_states)]
+BEGIN_PROVIDER [double precision, int_on_top_of_r_approx_svd, (N_states)]
  implicit none
  BEGIN_DOC 
 ! Numerical integration of the on top pair density approximated by a svd 
@@ -167,9 +167,9 @@ BEGIN_PROVIDER [double precision, integral_on_top_of_r_approx_svd, (N_states)]
  integer :: i,k
  double precision :: weight,wall_1,wall_2,wall_3,wall_4
  
- integral_on_top_of_r_approx_svd = 0.d0
+ int_on_top_of_r_approx_svd = 0.d0
 !call cpu_time(wall_3)
- provide singular_left_vec_approx_svd_two_dm_at_r
+ provide l_vec_approx_svd_two_dm_at_r
 !call cpu_time(wall_4)
 
 !print*,'cpu time SVD provinding = ',wall_4 - wall_3
@@ -179,7 +179,7 @@ BEGIN_PROVIDER [double precision, integral_on_top_of_r_approx_svd, (N_states)]
  do istate = 1, N_states
   do i = 1, n_points_final_grid
    weight = final_weight_at_r_vector(i)
-   integral_on_top_of_r_approx_svd(istate) += on_top_of_r_approx_svd(i,istate) * weight
+   int_on_top_of_r_approx_svd(istate) += on_top_of_r_approx_svd(i,istate) * weight
   enddo
  enddo
  call cpu_time(wall_2)
@@ -210,16 +210,16 @@ double precision function on_top_of_r_approx_svd_function(i,istate)
  integer, intent(in) :: i,istate
  integer :: k 
  on_top_of_r_approx_svd_function = 0.d0
- do k =1,n_singular_approx_svd_two_dm(istate)
-  on_top_of_r_approx_svd_function += singular_approx_svd_two_dm(k,istate) * singular_left_vec_approx_svd_two_dm_at_r(k,i,istate) * singular_right_vec_approx_svd_two_dm_at_r(k,i,istate) 
+ do k =1,n_approx_svd_two_dm(istate)
+  on_top_of_r_approx_svd_function += approx_svd_two_dm(k,istate) * l_vec_approx_svd_two_dm_at_r(k,i,istate) * r_vec_approx_svd_two_dm_at_r(k,i,istate) 
  enddo
 end
 
 
- BEGIN_PROVIDER [integer, n_singular_approx_svd_two_dm_correlation, (N_states)]
-&BEGIN_PROVIDER [integer, n_max_singular_approx_svd_two_dm_correlation]
+ BEGIN_PROVIDER [integer, n_approx_svd_two_dm_correl, (N_states)]
+&BEGIN_PROVIDER [integer, n_max_approx_svd_two_dm_correl]
  BEGIN_DOC 
-! Number of singular vectors (left and right) kept to represent the two-body tensor of each states
+! Number of svd vectors (l and r) kept to represent the two-body tensor of each states
 ! This number depends on thr_ontop_approx 
  END_DOC
  implicit none
@@ -265,30 +265,30 @@ end
   call wall_time(wall_2)
  print*,'*****SVD is done ******' 
  print*,'Wall time for SVD = ',wall_2 - wall_1
-  n_singular_approx_svd_two_dm_correlation(istate) =1 
-  print*,n_singular_approx_svd_two_dm_correlation(istate),D_i(n_singular_approx_svd_two_dm_correlation(istate))
-  do while ( (dabs(D_i(n_singular_approx_svd_two_dm_correlation(istate))) .gt. thresh) .AND. (n_singular_approx_svd_two_dm_correlation(istate) .lt. mo_tot_num**2)  )
-   n_singular_approx_svd_two_dm_correlation(istate) += 1
-   print*,n_singular_approx_svd_two_dm_correlation(istate),D_i(n_singular_approx_svd_two_dm_correlation(istate))
+  n_approx_svd_two_dm_correl(istate) =1 
+  print*,n_approx_svd_two_dm_correl(istate),D_i(n_approx_svd_two_dm_correl(istate))
+  do while ( (dabs(D_i(n_approx_svd_two_dm_correl(istate))) .gt. thresh) .AND. (n_approx_svd_two_dm_correl(istate) .lt. mo_tot_num**2)  )
+   n_approx_svd_two_dm_correl(istate) += 1
+   print*,n_approx_svd_two_dm_correl(istate),D_i(n_approx_svd_two_dm_correl(istate))
   enddo
-  if (dabs(D_i(n_singular_approx_svd_two_dm_correlation(istate))) .lt. thresh) then
-   n_singular_approx_svd_two_dm_correlation(istate) -= 1
+  if (dabs(D_i(n_approx_svd_two_dm_correl(istate))) .lt. thresh) then
+   n_approx_svd_two_dm_correl(istate) -= 1
   endif
   print*,'************************'
-  print*,'n_eigen_approx_two_dm  =',n_singular_approx_svd_two_dm_correlation(istate)
+  print*,'n_eigen_approx_two_dm  =',n_approx_svd_two_dm_correl(istate)
   print*,'************************'
  enddo
- n_max_singular_approx_svd_two_dm_correlation = maxval(n_singular_approx_svd_two_dm_correlation)
+ n_max_approx_svd_two_dm_correl = maxval(n_approx_svd_two_dm_correl)
 
 END_PROVIDER
 
- BEGIN_PROVIDER [double precision, singular_approx_svd_two_dm_correlation, (n_max_singular_approx_svd_two_dm_correlation, N_states)]
-&BEGIN_PROVIDER [double precision, singular_left_vec_approx_svd_two_dm_correlation, (mo_tot_num**2,n_max_singular_approx_svd_two_dm_correlation,N_states)]
-&BEGIN_PROVIDER [double precision, singular_right_vec_approx_svd_two_dm_correlation, (mo_tot_num**2,n_max_singular_approx_svd_two_dm_correlation,N_states)]
+ BEGIN_PROVIDER [double precision, approx_svd_two_dm_correl, (n_max_approx_svd_two_dm_correl, N_states)]
+&BEGIN_PROVIDER [double precision, l_vec_approx_svd_two_dm_correl, (mo_tot_num**2,n_max_approx_svd_two_dm_correl,N_states)]
+&BEGIN_PROVIDER [double precision, r_vec_approx_svd_two_dm_correl, (mo_tot_num**2,n_max_approx_svd_two_dm_correl,N_states)]
  BEGIN_DOC 
- !singular_approx_svd_two_dm(i,istate) = ith Singular values to represent the two-body tensor of the state istate 
- !singular_left_vec_approx_svd_two_dm(I,i,istate) = component on the couple of MOs approx_two_dm_map(I,1) and approx_two_dm_map(I,2) of the ith Singular left vector of the state istate 
- !singular_right_vec_approx_svd_two_dm(I,i,istate) = component on the couple of MOs approx_two_dm_map(I,1) and approx_two_dm_map(I,2) of the ith Singular right vector of the state istate 
+ !approx_svd_two_dm(i,istate) = ith Singular values to represent the two-body tensor of the state istate 
+ !l_vec_approx_svd_two_dm(I,i,istate) = component on the couple of MOs approx_two_dm_map(I,1) and approx_two_dm_map(I,2) of the ith Singular l vector of the state istate 
+ !r_vec_approx_svd_two_dm(I,i,istate) = component on the couple of MOs approx_two_dm_map(I,1) and approx_two_dm_map(I,2) of the ith Singular r vector of the state istate 
  END_DOC
  implicit none
  integer :: i,j,k,l,ij,kl
@@ -319,11 +319,11 @@ END_PROVIDER
  
    call svd(mat_i,size(mat_i,1),u_i,size(u_i,1),D_i,vt_i,size(vt_i,1),size(mat_i,1),size(mat_i,2))
  
-  do i = 1,n_singular_approx_svd_two_dm_correlation(istate)
-   singular_approx_svd_two_dm_correlation(i,istate) = D_i(i)
+  do i = 1,n_approx_svd_two_dm_correl(istate)
+   approx_svd_two_dm_correl(i,istate) = D_i(i)
    do j = 1,mo_tot_num**2
-    singular_left_vec_approx_svd_two_dm_correlation(j,i,istate) = u_i(j,i)
-    singular_right_vec_approx_svd_two_dm_correlation(j,i,istate) = vt_i(i,j)
+    l_vec_approx_svd_two_dm_correl(j,i,istate) = u_i(j,i)
+    r_vec_approx_svd_two_dm_correl(j,i,istate) = vt_i(i,j)
    enddo 
   enddo
  enddo
@@ -331,25 +331,25 @@ END_PROVIDER
 END_PROVIDER
 
 
- BEGIN_PROVIDER [double precision, singular_left_vec_approx_svd_two_dm_at_r_correlation , (n_max_singular_approx_svd_two_dm_correlation,n_points_final_grid,N_states)]
-&BEGIN_PROVIDER [double precision, singular_right_vec_approx_svd_two_dm_at_r_correlation, (n_max_singular_approx_svd_two_dm_correlation,n_points_final_grid,N_states)]
+ BEGIN_PROVIDER [double precision, l_vec_approx_svd_two_dm_at_r_correl , (n_max_approx_svd_two_dm_correl,n_points_final_grid,N_states)]
+&BEGIN_PROVIDER [double precision, r_vec_approx_svd_two_dm_at_r_correl, (n_max_approx_svd_two_dm_correl,n_points_final_grid,N_states)]
  BEGIN_DOC 
- !singular_left_vec_approx_svd_two_dm_at_r(i,j,istate) = value in real space of the ith left singular vector at the jth point of the grid for the state istate
- !singular_right_vec_approx_svd_two_dm_at_r(i,j,istate) = value in real space of the ith right singular vector at the jth point of the grid for the state istate
+ !l_vec_approx_svd_two_dm_at_r(i,j,istate) = value in real space of the ith l svd vector at the jth point of the grid for the state istate
+ !r_vec_approx_svd_two_dm_at_r(i,j,istate) = value in real space of the ith r svd vector at the jth point of the grid for the state istate
  END_DOC
  implicit none
  integer :: i,j,k
 
- singular_left_vec_approx_svd_two_dm_at_r_correlation = 0d0
- singular_right_vec_approx_svd_two_dm_at_r_correlation = 0d0
+ l_vec_approx_svd_two_dm_at_r_correl = 0d0
+ r_vec_approx_svd_two_dm_at_r_correl = 0d0
  !!!!!!unfoldage!!!!!!!
  integer :: istate
  do istate = 1, N_states
   do i = 1,n_points_final_grid
-   do j = 1,n_singular_approx_svd_two_dm_correlation(istate) 
+   do j = 1,n_approx_svd_two_dm_correl(istate) 
     do k = 1,mo_tot_num**2
-     singular_left_vec_approx_svd_two_dm_at_r_correlation(j,i,istate) +=  singular_left_vec_approx_svd_two_dm_correlation(k,j,istate) * mos_in_r_array(approx_two_dm_map(k,1),i) * mos_in_r_array(approx_two_dm_map(k,2),i)
-     singular_right_vec_approx_svd_two_dm_at_r_correlation(j,i,istate)+= singular_right_vec_approx_svd_two_dm_correlation(k,j,istate) * mos_in_r_array(approx_two_dm_map(k,1),i) * mos_in_r_array(approx_two_dm_map(k,2),i) 
+     l_vec_approx_svd_two_dm_at_r_correl(j,i,istate) +=  l_vec_approx_svd_two_dm_correl(k,j,istate) * mos_in_r_array(approx_two_dm_map(k,1),i) * mos_in_r_array(approx_two_dm_map(k,2),i)
+     r_vec_approx_svd_two_dm_at_r_correl(j,i,istate)+= r_vec_approx_svd_two_dm_correl(k,j,istate) * mos_in_r_array(approx_two_dm_map(k,1),i) * mos_in_r_array(approx_two_dm_map(k,2),i) 
     enddo
    enddo
   enddo
@@ -357,7 +357,7 @@ END_PROVIDER
 
 END_PROVIDER
 
- BEGIN_PROVIDER [double precision, integral_on_top_of_r_approx_svd_correlation, (N_states)]
+ BEGIN_PROVIDER [double precision, int_on_top_of_r_approx_svd_correl, (N_states)]
  implicit none
  BEGIN_DOC 
  !Numerical integration of the on top pair density approximated by a svd 
@@ -365,49 +365,49 @@ END_PROVIDER
  integer :: i,k
  double precision :: weight,wall_1,wall_2,wall_3,wall_4
 
- provide singular_right_vec_approx_svd_two_dm_at_r_correlation  
+ provide r_vec_approx_svd_two_dm_at_r_correl  
  
  call cpu_time(wall_1)
  integer :: istate
  do istate = 1, N_states
   do i = 1, n_points_final_grid
    weight = final_weight_at_r_vector(i)
-   integral_on_top_of_r_approx_svd_correlation(istate) += on_top_of_r_approx_svd_correlation(i,istate) * weight
+   int_on_top_of_r_approx_svd_correl(istate) += on_top_of_r_approx_svd_correl(i,istate) * weight
   enddo
  enddo
  call cpu_time(wall_2)
- print*,'cpu time SVD correlation approx = ',wall_2 - wall_1
+ print*,'cpu time SVD correl approx = ',wall_2 - wall_1
 END_PROVIDER
 
- BEGIN_PROVIDER [double precision, on_top_of_r_approx_svd_correlation, (n_points_final_grid,N_states)]
+ BEGIN_PROVIDER [double precision, on_top_of_r_approx_svd_correl, (n_points_final_grid,N_states)]
  implicit none
  BEGIN_DOC 
  !on top pair density computed with the SVD approximation 
  END_DOC 
  integer :: i,istate
- double precision :: on_top_of_r_approx_svd_correlation_function
+ double precision :: on_top_of_r_approx_svd_correl_function
  double precision :: r(3)
  double precision :: dm_a,dm_b 
  ! initialization for paralellization 
- on_top_of_r_approx_svd_correlation(1,1) = on_top_of_r_approx_svd_correlation_function(1,1)
+ on_top_of_r_approx_svd_correl(1,1) = on_top_of_r_approx_svd_correl_function(1,1)
  do istate = 1, N_states
   do i= 1, n_points_final_grid
    dm_a = one_body_dm_alpha_at_r(i,istate)
    dm_b = one_body_dm_beta_at_r(i,istate)
-   on_top_of_r_approx_svd_correlation(i,istate) = dm_a*dm_b + on_top_of_r_approx_svd_correlation_function(i,istate)
+   on_top_of_r_approx_svd_correl(i,istate) = dm_a*dm_b + on_top_of_r_approx_svd_correl_function(i,istate)
   enddo
  enddo
 END_PROVIDER 
 
-double precision function on_top_of_r_approx_svd_correlation_function(i,istate)
+double precision function on_top_of_r_approx_svd_correl_function(i,istate)
  implicit none
  BEGIN_DOC
  !evaluation at a given point of the grid of the on top pair density approximated by a svd
  END_DOC
  integer, intent(in) :: i,istate
  integer :: k
- on_top_of_r_approx_svd_correlation_function = 0.d0
- do k =1,n_singular_approx_svd_two_dm_correlation(istate)
-  on_top_of_r_approx_svd_correlation_function += singular_approx_svd_two_dm_correlation(k,istate) * singular_left_vec_approx_svd_two_dm_at_r_correlation(k,i,istate) * singular_right_vec_approx_svd_two_dm_at_r_correlation(k,i,istate) 
+ on_top_of_r_approx_svd_correl_function = 0.d0
+ do k =1,n_approx_svd_two_dm_correl(istate)
+  on_top_of_r_approx_svd_correl_function += approx_svd_two_dm_correl(k,istate) * l_vec_approx_svd_two_dm_at_r_correl(k,i,istate) * r_vec_approx_svd_two_dm_at_r_correl(k,i,istate) 
  enddo
 end
