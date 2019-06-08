e43db92089bc731ed42310aacb071ba79ee22bba
Emmanuel Giner <giner.emmanuel@gmail.com>
2019-06-08 16:35:13 +0200

    modifs in LYP on top effective dens

--- stable/dft_mu_of_r/on_top_based_functionals.irp.f
+++ stable/dft_mu_of_r/on_top_based_functionals.irp.f
@@ -120,8 +120,6 @@ subroutine give_epsilon_lyp_ontop_effective_spin_dens_provider(mu,i_point,eps_c_
   do istate = 1, N_states
    ! total density 
    rhoc = one_e_dm_and_grad_alpha_in_r(4,i_point,istate) + one_e_dm_and_grad_beta_in_r(4,i_point,istate) 
-   ! effective spin density 
-   rhoo = effective_spin_dm(i_point,istate)
    ! gradients of the effective spin density 
    grad_rho_a_2 = 0.D0
    grad_rho_b_2 = 0.D0
@@ -132,6 +130,8 @@ subroutine give_epsilon_lyp_ontop_effective_spin_dens_provider(mu,i_point,eps_c_
     grad_rho_a_b += grad_effective_alpha_dm(m,i_point,istate) * grad_effective_beta_dm(m,i_point,istate)
    enddo
    grad_rho_2 = grad_rho_a_2 + grad_rho_b_2 + 2.D0 * grad_rho_a_b
+
+   ! effective alpha and beta density
    rho_a = effective_alpha_dm(i_point,istate)
    rho_b = effective_beta_dm(i_point,istate)
 
@@ -139,11 +139,15 @@ subroutine give_epsilon_lyp_ontop_effective_spin_dens_provider(mu,i_point,eps_c_
    e_LYP = ec_lyp_88(rhoc,rho_a,rho_b,grad_rho_a_2,grad_rho_b_2,grad_rho_2)
    two_dm = core_inact_act_on_top_of_r(i_point,istate) ! on top of the wave function 
    two_dm_corr = on_top_two_dm_in_r_mu_corrected_from_two_dm(mu,istate,two_dm) ! extrapolated "exact" on top
+
    if(dabs(( (-2.d0+sqrt(2d0))*sqrt(2.d0*pi)*2.d0*two_dm_corr )).lt.1.d-12)cycle
+
    beta = (3.d0*e_LYP)/( (-2.d0+sqrt(2d0))*sqrt(2.d0*pi)*2.d0*two_dm_corr )
    ! Ecmd functional with the extrapolated exact on top when mu -> infty 
    ! and the usual PBE functional when mu = 0
+
    eps_c_md_ontop_LYP(istate)=e_LYP/(1.d0+beta*mu**3)
+  !endif
   enddo
 
 end
