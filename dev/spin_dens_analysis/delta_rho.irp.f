120e29902089baf312194a6ca83ca4c0b910d833
Emmanuel Giner LCT <eginer@obelixdev.local>
2019-11-21 17:26:45 +0100

    added spin_dens_analysis

---
+++ dev/spin_dens_analysis/delta_rho.irp.f
@@ -0,0 +1,149 @@
+ BEGIN_PROVIDER [integer, spin_dens_coord]
+ implicit none
+ BEGIN_DOC
+ !coordinate on which you are going to plot the spin density
+ !and integrate over the ohters
+ !spin_dens_coord = 1  === X
+ !spin_dens_coord = 2  === Y
+ !spin_dens_coord = 3  === Z
+ END_DOC
+ spin_dens_coord = 3
+ END_PROVIDER 
+
+
+ BEGIN_PROVIDER [double precision, delta_z]
+&BEGIN_PROVIDER [double precision, z_min]
+&BEGIN_PROVIDER [double precision, z_max]
+ implicit none
+ z_min = 0.d0
+ z_max = 10.d0
+ delta_z = 0.05d0
+END_PROVIDER
+
+BEGIN_PROVIDER [integer, N_z_pts]
+ implicit none
+ N_z_pts = int( (z_max - z_min)/delta_z )
+ print*,'N_z_pts = ',N_z_pts
+END_PROVIDER
+
+
+BEGIN_PROVIDER [double precision, integrated_delta_rho_all_points, (N_z_pts)]
+ BEGIN_DOC
+! 
+! integrated_rho(alpha,z) - integrated_rho(beta,z) for all the z points 
+! chosen
+!
+ END_DOC
+ implicit none
+ integer :: i,j,k,l,i_z,h
+ double precision :: z,function_integrated_delta_rho,c_k,c_j,n_i_h,accu
+ integrated_delta_rho_all_points = 0.d0
+ !$OMP PARALLEL DO DEFAULT(none) &
+ !$OMP PRIVATE(i,h,j,k,c_j,c_k,n_i_h,i_z) &
+ !$OMP SHARED(mo_num,ao_num,mo_coef, &
+ !$OMP   ao_integrated_delta_rho_all_points,one_e_spin_density_mo,integrated_delta_rho_all_points,N_z_pts)          
+ do i_z = 1, N_z_pts
+  do i = 1, mo_num
+    do h = 1, mo_num
+     n_i_h = one_e_spin_density_mo(i,h)
+     if(dabs(n_i_h).lt.1.d-10)cycle
+     do j = 1, ao_num
+      c_j = mo_coef(j,i)   ! coefficient of the ith MO on the jth AO
+      do k = 1, ao_num
+       c_k = mo_coef(k,h)   ! coefficient of the hth MO on the kth AO
+       integrated_delta_rho_all_points(i_z) += c_k * c_j * n_i_h *  ao_integrated_delta_rho_all_points(j,k,i_z)
+      enddo
+     enddo
+    enddo
+  enddo
+ enddo
+ !$OMP END PARALLEL DO
+
+ z = z_min
+ accu = 0.d0
+ do i = 1, N_z_pts
+  accu += integrated_delta_rho_all_points(i)
+  write(i_unit_integrated_delta_rho,*)z,integrated_delta_rho_all_points(i),accu
+  z += delta_z
+ enddo
+ print*,'sum of integrated_delta_rho = ',accu
+
+END_PROVIDER
+
+
+
+
+BEGIN_PROVIDER [ double precision, ao_integrated_delta_rho_all_points, (ao_num, ao_num, N_z_pts)]
+ BEGIN_DOC
+!  array of the overlap in x,y between the AO function and integrated between [z,z+dz] in the z axis
+!  for all the z points that are given (N_z_pts)
+ END_DOC
+  implicit none
+  integer :: i,j,n,l
+  double precision :: f,accu
+  integer :: dim1
+  double precision :: overlap, overlap_x, overlap_y, overlap_z
+  double precision :: alpha, beta, c
+  double precision :: A_center(3), B_center(3)
+  integer :: power_A(3), power_B(3)
+  integer :: i_z
+  double precision :: z,SABpartial,accu_x,accu_y,accu_z
+  dim1=100
+ z = z_min
+ do i_z = 1, N_z_pts
+ !$OMP PARALLEL DO DEFAULT(none) &
+ !$OMP PRIVATE(i,j,n,l,A_center,power_A,B_center,power_B,accu_z, &
+ !$OMP  overlap_x,overlap_y,overlap_z,overlap,c,alpha,beta) &
+ !$OMP SHARED(ao_num,nucl_coord,ao_nucl,ao_power,ao_prim_num,ao_expo_ordered_transp,ao_coef_normalized_ordered_transp, &
+ !$OMP   ao_integrated_delta_rho_all_points,N_z_pts,dim1,i_z,z,delta_z,spin_dens_coord)           
+  do j=1,ao_num
+   A_center(1) = nucl_coord( ao_nucl(j), 1 )
+   A_center(2) = nucl_coord( ao_nucl(j), 2 )
+   A_center(3) = nucl_coord( ao_nucl(j), 3 )
+   power_A(1)  = ao_power( j, 1 )
+   power_A(2)  = ao_power( j, 2 )
+   power_A(3)  = ao_power( j, 3 )
+   do i= 1,ao_num
+    B_center(1) = nucl_coord( ao_nucl(i), 1 )
+    B_center(2) = nucl_coord( ao_nucl(i), 2 )
+    B_center(3) = nucl_coord( ao_nucl(i), 3 )
+    power_B(1)  = ao_power( i, 1 )
+    power_B(2)  = ao_power( i, 2 )
+    power_B(3)  = ao_power( i, 3 )
+
+     accu_z = 0.d0
+     do n = 1,ao_prim_num(j)
+      alpha = ao_expo_ordered_transp(n,j)
+      do l = 1, ao_prim_num(i)
+       beta = ao_expo_ordered_transp(l,i)
+       call overlap_gaussian_xyz(A_center,B_center,alpha,beta,power_A,power_B,overlap_x,overlap_y,overlap_z,overlap,dim1)
+
+       c = ao_coef_normalized_ordered_transp(n,j) * ao_coef_normalized_ordered_transp(l,i) 
+       if(spin_dens_coord ==1 )then
+        accu_z += c* overlap_y * overlap_z * SABpartial(z,z+delta_z,A_center,B_center,power_A,power_B,alpha,beta,spin_dens_coord)
+       else if (spin_dens_coord ==2 )then
+        accu_z += c* overlap_x * overlap_z * SABpartial(z,z+delta_z,A_center,B_center,power_A,power_B,alpha,beta,spin_dens_coord)
+       else if (spin_dens_coord ==3 )then
+        accu_z += c* overlap_x * overlap_y * SABpartial(z,z+delta_z,A_center,B_center,power_A,power_B,alpha,beta,spin_dens_coord)
+       endif
+      enddo
+     enddo
+     ao_integrated_delta_rho_all_points(i,j,i_z) = accu_z
+   enddo
+  enddo
+ !$OMP END PARALLEL DO
+  z += delta_z
+ enddo
+END_PROVIDER
+
+BEGIN_PROVIDER [integer, i_unit_integrated_delta_rho]
+ implicit none
+ BEGIN_DOC
+! fortran unit for the writing of the integrated delta_rho
+ END_DOC
+ integer :: getUnitAndOpen
+ character*(128) :: output_i_unit_integrated_delta_rho
+ output_i_unit_integrated_delta_rho=trim(ezfio_filename)//'/delta_rho'
+ i_unit_integrated_delta_rho= getUnitAndOpen(output_i_unit_integrated_delta_rho,'w')
+
+END_PROVIDER
---
+++ dev/spin_dens_analysis/spin_mulliken.irp.f
@@ -0,0 +1,151 @@
+
+BEGIN_PROVIDER [double precision, spin_population, (ao_num,ao_num)]
+ implicit none
+ integer :: i,j
+ BEGIN_DOC
+! spin population on the ao basis :
+! spin_population(i,j) = rho_AO(alpha)(i,j) - rho_AO(beta)(i,j) * <AO_i|AO_j>
+ END_DOC
+ spin_population = 0.d0
+ do i = 1, ao_num
+  do j = 1, ao_num
+   spin_population(j,i) = one_e_spin_density_ao(i,j) * ao_overlap(i,j)
+  enddo
+ enddo
+END_PROVIDER
+
+ BEGIN_PROVIDER [double precision, spin_population_angular_momentum, (0:ao_l_max)]
+&BEGIN_PROVIDER [double precision, spin_population_angular_momentum_per_atom, (0:ao_l_max,nucl_num)]
+ implicit none
+ integer :: i
+ double precision :: accu
+ spin_population_angular_momentum = 0.d0
+ spin_population_angular_momentum_per_atom = 0.d0
+ do i = 1,  ao_num
+  spin_population_angular_momentum(ao_l(i)) += spin_gross_orbital_product(i)
+  spin_population_angular_momentum_per_atom(ao_l(i),ao_nucl(i)) += spin_gross_orbital_product(i)
+ enddo
+
+END_PROVIDER 
+
+
+BEGIN_PROVIDER [double precision, spin_gross_orbital_product, (ao_num)]
+ implicit none
+ spin_gross_orbital_product = 0.d0
+ integer :: i,j
+ BEGIN_DOC
+! gross orbital product for the spin population
+ END_DOC
+ do i = 1, ao_num
+  do j = 1, ao_num
+   spin_gross_orbital_product(i) += spin_population(j,i)
+  enddo
+ enddo
+
+END_PROVIDER
+
+BEGIN_PROVIDER [double precision, mulliken_spin_densities, (nucl_num)]
+ implicit none
+ integer :: i,j
+ BEGIN_DOC
+!ATOMIC SPIN POPULATION (ALPHA MINUS BETA)
+ END_DOC
+ mulliken_spin_densities = 0.d0
+ do i = 1, ao_num
+  mulliken_spin_densities(ao_nucl(i)) += spin_gross_orbital_product(i)
+ enddo
+
+END_PROVIDER
+
+ BEGIN_PROVIDER [double precision, electronic_population_alpha, (ao_num,ao_num)]
+&BEGIN_PROVIDER [double precision, electronic_population_beta, (ao_num,ao_num)]
+ implicit none
+ integer :: i,j
+ BEGIN_DOC
+! spin population on the ao basis :
+! spin_population(i,j) = rho_AO(alpha)(i,j) - rho_AO(beta)(i,j) * <AO_i|AO_j>
+ END_DOC
+ electronic_population_alpha = 0.d0
+ electronic_population_beta = 0.d0
+ do i = 1, ao_num
+  do j = 1, ao_num
+   electronic_population_alpha(j,i) = one_e_dm_mo_alpha_average(i,j) * ao_overlap(i,j)
+   electronic_population_beta(j,i) = one_e_dm_mo_beta_average(i,j) * ao_overlap(i,j)
+  enddo
+ enddo
+
+END_PROVIDER
+
+ BEGIN_PROVIDER [double precision, gross_orbital_product_alpha, (ao_num)]
+&BEGIN_PROVIDER [double precision, gross_orbital_product_beta, (ao_num)]
+ implicit none
+ spin_gross_orbital_product = 0.d0
+ integer :: i,j
+ BEGIN_DOC
+! gross orbital product 
+ END_DOC
+ do i = 1, ao_num
+  do j = 1, ao_num
+   gross_orbital_product_alpha(i) += electronic_population_alpha(j,i)
+   gross_orbital_product_beta(i) += electronic_population_beta(j,i)
+  enddo
+ enddo
+
+END_PROVIDER
+
+ BEGIN_PROVIDER [double precision, mulliken_densities_alpha, (nucl_num)]
+&BEGIN_PROVIDER [double precision, mulliken_densities_beta, (nucl_num)]
+ implicit none
+ integer :: i,j
+ BEGIN_DOC
+!
+ END_DOC
+ mulliken_densities_alpha = 0.d0
+ mulliken_densities_beta = 0.d0
+ do i = 1, ao_num
+  mulliken_densities_alpha(ao_nucl(i)) += gross_orbital_product_alpha(i)
+  mulliken_densities_beta(ao_nucl(i)) += gross_orbital_product_beta(i)
+ enddo
+
+END_PROVIDER
+
+
+subroutine print_mulliken_sd
+ implicit none
+ double precision :: accu
+ integer :: i
+ integer :: j
+ print*,'Mulliken spin densities'
+ accu= 0.d0
+ do i = 1, nucl_num
+  print*,i,nucl_charge(i),mulliken_spin_densities(i)
+  accu += mulliken_spin_densities(i)
+ enddo
+ print*,'Sum of Mulliken SD = ',accu
+ print*,'AO SPIN POPULATIONS'
+ accu = 0.d0
+ do i = 1, ao_num
+  accu += spin_gross_orbital_product(i)
+  write(*,'(1X,I3,1X,A4,1X,I2,1X,A4,1X,F10.7)')i,trim(element_name(int(nucl_charge(ao_nucl(i))))),ao_nucl(i),trim(l_to_character(ao_l(i))),spin_gross_orbital_product(i)
+ enddo
+ print*,'sum = ',accu
+ accu = 0.d0
+ print*,'Angular momentum analysis'
+ do i = 0,  ao_l_max
+  accu += spin_population_angular_momentum(i)
+  print*,' ',trim(l_to_character(i)),spin_population_angular_momentum(i)
+ print*,'sum = ',accu
+ enddo
+ print*,'Angular momentum analysis per atom'
+ print*,'Angular momentum analysis'
+ do j = 1,nucl_num
+  accu = 0.d0
+  do i = 0,  ao_l_max
+   accu += spin_population_angular_momentum_per_atom(i,j)
+   write(*,'(1X,I3,1X,A4,1X,A4,1X,F10.7)')j,trim(element_name(int(nucl_charge(j)))),trim(l_to_character(i)),spin_population_angular_momentum_per_atom(i,j)
+   print*,'sum = ',accu
+  enddo
+ enddo
+
+end
+
