72d0d5bd55554b84dbb9d8f795dc637e9930753c
dtraore97 <diatatraore@yahoo.com>
2020-05-05 20:53:22 +0200

    Add print_on_top.irp.f and modif print_ecmd_components.irp.f

--- stable/func_ecmd_utils/rout_pbe_ot.irp.f
+++ stable/func_ecmd_utils/rout_pbe_ot.irp.f
@@ -116,7 +116,7 @@
   end subroutine ecmdsrPBEn2
 
 
-  subroutine exmdsrPBEn2(mu_in,rho_a,rho_b,grad_rho_a_2,grad_rho_b_2,grad_rho_2,grad_rho_a_b,rho2,ex_srmuPBE,dexdrho_a,dexdrho_b, dexdrho, dexdgrad_rho_a_2,dexdgrad_rho_b_2,dexdgrad_rho_a_b, dexdgrad_rho_2,dexdrho2)
+  subroutine exmdsrPBEn2(mu_in,rho_a,rho_b,grad_rho_a_2,grad_rho_b_2,grad_rho_2,grad_rho_a_b,rho2,ex_srmuPBE)
 
   implicit none
   BEGIN_DOC
@@ -126,25 +126,15 @@
  
   double precision, intent(in)  :: mu_in
   double precision, intent(in)  :: rho_a,rho_b,grad_rho_a_2,grad_rho_b_2,grad_rho_a_b, rho2
-  double precision, intent(out) :: ex_srmuPBE,dexdrho_a,dexdrho_b,dexdgrad_rho_a_2,dexdgrad_rho_b_2,dexdgrad_rho_a_b, dexdgrad_rho_2, dexdrho, dexdrho2!, dexdrho2_a, dexdrho2_b
+  double precision, intent(out) :: ex_srmuPBE
   double precision              :: mu
   double precision              :: exPBE,dexPBEdrho_a,dexPBEdrho_b,dexPBEdrho, dexPBEdgrad_rho_a_2,dexPBEdgrad_rho_b_2,dexPBEdgrad_rho_a_b,dexPBEdgrad_rho_2
-  double precision              :: denom, ddenomdrho, ddenomdgrad_rho_2, ddenomdrho2
-  double precision              :: delta, ddeltadrho, ddeltadgrad_rho_2, ddeltadrho2
-  double precision              :: gamma, dgammadrho, dgammadgrad_rho_2, dgammadrho2
+  double precision              :: denom, delta, gamma
   double precision              :: grad_rho_2
   double precision              :: pi, a, b, thr
   double precision              :: rho, m  
-  double precision              :: dn2xc_UEGdrho, dn2xc_UEGdrho2, n2xc_UEG 
+  double precision              :: n2xc
   ex_srmuPBE       = 0.d0 
-  dexdrho_a        = 0.d0
-  dexdrho_b        = 0.d0
-  dexdrho          = 0.d0
-  dexdgrad_rho_a_2 = 0.d0 
-  dexdgrad_rho_b_2 = 0.d0
-  dexdgrad_rho_a_b = 0.d0  
-  dexdgrad_rho_2   = 0.d0
-  dexdrho2         = 0.d0
   rho = rho_a + rho_b
   if(rho.lt.1.d-10)then
    return
@@ -162,14 +152,14 @@
 
 ! calculation of energy
   a = pi / 2.d0
-  b = 2*dsqrt(pi)*(2*dsqrt(2.d0) - 1.d0)/3.d0   
+  b = 2.d0*dsqrt(pi)*(2.d0*dsqrt(2.d0) - 1.d0)/3.d0   
    
-  n2xc_UEG = rho2 - rho**2
-  if(dabs(n2xc_UEG).lt.thr)then
-   n2xc_UEG = 1.d-12
+  n2xc = rho2 - rho**2
+  if(dabs(n2xc).lt.thr)then
+   n2xc = 1.d-12
   endif
 
-  gamma = exPBE / (a*n2xc_UEG)
+  gamma = exPBE / (a*n2xc)
   if(dabs(gamma).lt.thr)then
    gamma = 1.d-12
   endif
@@ -190,62 +180,6 @@
    stop
   endif
 
-! calculation of derivatives 
-  !dec/dn
-  dexPBEdrho = 0.5d0 *(dexPBEdrho_a + dexPBEdrho_b)
-
-  dn2xc_UEGdrho = -2.d0*rho
-  
-  if(dabs(rho2).lt.1.d-10)then
-  dgammadrho = - dexPBEdrho/(a*rho**2) - (exPBE*dn2xc_UEGdrho)/(a*rho**4)
-  else
-  dgammadrho = dexPBEdrho/(a*n2xc_UEG) - (exPBE*dn2xc_UEGdrho)/(a*n2xc_UEG**2)
-  endif
-
-  ddeltadrho = (b*rho2*gamma**2)*dexPBEdrho/(exPBE**2) - (b*rho2/exPBE)*2.d0*gamma*dgammadrho
-
-  denom      = 1.d0 + delta*mu + gamma*(mu**2)
-  ddenomdrho = ddeltadrho*mu + dgammadrho*mu
-
-  dexdrho    = dexPBEdrho/denom - exPBE*ddenomdrho/(denom**2)
-  dexdrho_a  = dexdrho
-  dexdrho_b  = dexdrho
-
-  !dec/((dgradn)^2)
-  dexPBEdgrad_rho_2 = 0.25d0 *(dexPBEdgrad_rho_a_2 + dexPBEdgrad_rho_b_2 + dexPBEdgrad_rho_a_b) 
- 
-  if(dabs(rho2).lt.1.d-10)then
-  dgammadgrad_rho_2  = - dexPBEdgrad_rho_2/(a*rho**2)
-  else
-  dgammadgrad_rho_2  = dexPBEdgrad_rho_2/(a*n2xc_UEG)
-  endif
-
-  ddeltadgrad_rho_2 = (b*rho*gamma**2)*dexPBEdgrad_rho_2/(exPBE**2) -(b*rho2*2.d0*gamma*dgammadgrad_rho_2)/exPBE
-
-  ddenomdgrad_rho_2  = ddeltadgrad_rho_2*mu + dgammadgrad_rho_2*mu**2
-  
-  dexdgrad_rho_2     = dexPBEdgrad_rho_2/denom - exPBE*ddenomdgrad_rho_2/(denom**2)
-  dexdgrad_rho_a_2   = dexdgrad_rho_2 ! + decdgrad_n_m + decdgrad_m_2
-  dexdgrad_rho_b_2   = dexdgrad_rho_2 ! - decdgrad_n_m + decdgrad_m_2
-  dexdgrad_rho_a_b   = 2.d0*dexdgrad_rho_2 ! - 2.d0*decdgrad_m_2 
-
-  !dec/dn2
- 
-  dn2xc_UEGdrho2 = 1.d0
-  if(dabs(rho2).gt.1.d-10)then
-   dgammadrho2    = - exPBE*dn2xc_UEGdrho2/(a*(-rho**2)**2)
-  else 
-   dgammadrho2    = - exPBE*dn2xc_UEGdrho2/(a*n2xc_UEG**2)
-  endif
-
-  ddeltadrho2    = -(b*gamma**2)/exPBE -(b*rho2*2.d0*gamma*dgammadrho2)/exPBE
-  
-  ddenomdrho2 = ddeltadrho2*mu + dgammadrho2*mu**2
-
-  dexdrho2 = - exPBE*ddenomdrho2/(denom**2)
-  ! dexdrho2_a = dexdrho2
-  ! dexdrho2_b = dexdrho2
- 
   end subroutine exmdsrPBEn2
 
 
--- stable/rsdft_ecmd/routine_print.irp.f
+++ stable/rsdft_ecmd/routine_print.irp.f
@@ -33,6 +33,8 @@
 
  BEGIN_PROVIDER [double precision, int_exmdsrpbe_n2_exact,(N_states)]
 &BEGIN_PROVIDER [double precision, int_exmdsrpbe_n2_extrapolated, (N_states)]
+&BEGIN_PROVIDER [double precision, int_exmdsrpbe_n2_UEG, (N_states)]
+
  implicit none
  BEGIN_DOC
  ! Integrate the exmd_sr_pbe using the exact on-top and the extrapolated one
@@ -40,20 +42,28 @@
  END_DOC
   integer          :: m, istate, ipoint
   double precision :: weight
-  double precision :: mu, mu_correction_of_on_top, on_top, on_top_extrap
-  double precision :: rho_a, rho_b, grad_rho_a(3),grad_rho_b(3),grad_rho_2, grad_rho_a_2, grad_rho_b_2, grad_rho_a_b
-  double precision :: ex_srmuPBE, dexdrho_a, dexdrho_b, dexdrho, dexdgrad_rho_a_2, dexdgrad_rho_b_2, dexdgrad_rho_a_b, dexdgrad_rho_2, dexdrho2
-  
-  mu = mu_erf_dft  
+  double precision :: mu, mu_correction_of_on_top, on_top, on_top_extrap, on_top_UEG
+  double precision :: rho, rho_a, rho_b, grad_rho_a(3),grad_rho_b(3),grad_rho_2, grad_rho_a_2, grad_rho_b_2, grad_rho_a_b
+  double precision :: ex_srmuPBE
+  double precision :: g0, dg0drho
+  double precision :: r(3), r_norm, f_psi
+  do istate = 1, N_states
   int_exmdsrpbe_n2_exact        = 0.d0
   int_exmdsrpbe_n2_extrapolated = 0.d0
-  
-  do istate = 1, N_states
+  int_exmdsrpbe_n2_UEG          = 0.d0
   do ipoint = 1, n_points_final_grid
+   
+   mu = mu_erf_dft 
+   !call give_mu_of_r_cas(r,istate,mu,f_psi,on_top)
    weight = final_weight_at_r_vector(ipoint)
+   r(1) = final_grid_points(1,ipoint)
+   r(2) = final_grid_points(2,ipoint)
+   r(3) = final_grid_points(3,ipoint)
+   r_norm = dsqrt(r(1)**2 + r(2)**2 + r(3)**2)
 
    rho_a = one_e_dm_and_grad_alpha_in_r(4,ipoint,istate)
    rho_b = one_e_dm_and_grad_beta_in_r(4,ipoint,istate)
+   rho = rho_a + rho_b
    grad_rho_a(1:3) = one_e_dm_and_grad_alpha_in_r(1:3,ipoint,istate)
    grad_rho_b(1:3) = one_e_dm_and_grad_beta_in_r(1:3,ipoint,istate)
    grad_rho_a_2 = 0.d0
@@ -65,16 +75,27 @@
     grad_rho_a_b += grad_rho_a(m) * grad_rho_b(m)
    enddo
 
-   on_top = on_top_cas_mu_r(ipoint,istate) ! we use mu = cas_ful
+   !on_top = on_top_cas_mu_r(ipoint,istate) ! we use mu = cas_ful
+   on_top = total_cas_on_top_density(ipoint,istate)
 !  We take the extrapolated on-top pair density (Eq. 29)
 !  Multiplied by 2 because of difference of normalizations between the on_top of QP2 and that of JCP, 150, 084103 1-10 (2019)
    on_top_extrap = 2.d0 * mu_correction_of_on_top(mu,on_top)
-   
-     call exmdsrPBEn2(mu,rho_a,rho_b,grad_rho_a_2,grad_rho_b_2,grad_rho_2,grad_rho_a_b,on_top,ex_srmuPBE,dexdrho_a,dexdrho_b, dexdrho, dexdgrad_rho_a_2,dexdgrad_rho_b_2,dexdgrad_rho_a_b, dexdgrad_rho_2,dexdrho2)
+   call g0_dg0(rho, rho_a, rho_b, g0, dg0drho)
+   on_top_UEG = (rho**2)*g0
+  !ON-TOP-EXACT 
+   call exmdsrPBEn2(mu,rho_a,rho_b,grad_rho_a_2,grad_rho_b_2,grad_rho_2,grad_rho_a_b,on_top,ex_srmuPBE)
    int_exmdsrpbe_n2_exact += ex_srmuPBE*weight
 
-   call exmdsrPBEn2(mu,rho_a,rho_b,grad_rho_a_2,grad_rho_b_2,grad_rho_2,grad_rho_a_b,on_top_extrap,ex_srmuPBE,dexdrho_a,dexdrho_b, dexdrho, dexdgrad_rho_a_2,dexdgrad_rho_b_2,dexdgrad_rho_a_b, dexdgrad_rho_2,dexdrho2)
+  !ON-TOP EXTRAP
+   call exmdsrPBEn2(mu,rho_a,rho_b,grad_rho_a_2,grad_rho_b_2,grad_rho_2,grad_rho_a_b,on_top_extrap,ex_srmuPBE)
    int_exmdsrpbe_n2_extrapolated += ex_srmuPBE*weight
+
+  !ON-TOP UEG
+   call exmdsrPBEn2(mu,rho_a,rho_b,grad_rho_a_2,grad_rho_b_2,grad_rho_2,grad_rho_a_b,on_top_UEG,ex_srmuPBE)
+   int_exmdsrpbe_n2_UEG += ex_srmuPBE*weight
+
+   !testr:
+   !write(33,*) r(1), on_top, on_top_extrap, on_top_UEG, on_top_UEG
   enddo
  enddo
  END_PROVIDER
@@ -110,6 +131,7 @@
   write(*, '(A22,X,F16.10)') 'ecmd_pbe_on_top_at_mu    = ',ecmd_pbe_on_top_at_mu
   write(*, '(A22,X,F16.10)') 'exmdsr_pbe_on_top_exact  = ',int_exmdsrpbe_n2_exact
   write(*, '(A22,X,F16.10)') 'exmdsr_pbe_on_top_extra  = ',int_exmdsrpbe_n2_extrapolated
+  write(*, '(A22,X,F16.10)') 'exmdsr_pbe_on_top_UEG  = ',int_exmdsrpbe_n2_UEG
   print*,'On-top-UEG functional '
   write(*, '(A22,X,F16.10)') 'Ecmd PBE-UEG             = ',ecmd_pbe_ueg_prov
   write(*, '(A22,X,F16.10)') 'energy_c_md_sr_pbe       = ',energy_c_md_sr_pbe
