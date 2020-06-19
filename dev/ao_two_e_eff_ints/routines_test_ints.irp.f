58c76cf3c1a82c5a96f779c9ba6e1be8f247c195
Emmanuel Giner <giner.emmanuel@gmail.com>
2020-06-19 17:39:11 +0200

    integrals erf at least as fast as before

--- dev/ao_two_e_eff_ints/routines_test_ints.irp.f
+++ dev/ao_two_e_eff_ints/routines_test_ints.irp.f
@@ -3,20 +3,30 @@ subroutine test_new_erf_ints
  integer :: i,j,k,l
  double precision :: ao_two_e_integral_schwartz_accel_erf_new,ao_two_e_integral_schwartz_accel_erf
  double precision :: ref,new
+ double precision :: wall0,wall1
+ call wall_time(wall0)
  do i = 1, ao_num
   do j = 1, ao_num
    do k = 1, ao_num
     do l = 1, ao_num
      ref = ao_two_e_integral_schwartz_accel_erf(i,j,k,l)
-     new = ao_two_e_integral_schwartz_accel_erf_new(i,j,k,l)
-     if(dabs(ref - new).gt.1.d-14)then
-       print*,'Problemmmmmm'
-       print*,i,j,k,l
-       stop
-     endif
     enddo
    enddo
   enddo
  enddo
+ call wall_time(wall1)
+ print*,'wall time old = ',wall1 - wall0
+ call wall_time(wall0)
+ do i = 1, ao_num
+  do j = 1, ao_num
+   do k = 1, ao_num
+    do l = 1, ao_num
+     new = ao_two_e_integral_schwartz_accel_erf_new(i,j,k,l)
+    enddo
+   enddo
+  enddo
+ enddo
+ call wall_time(wall1)
+ print*,'wall time new = ',wall1 - wall0
 
 end
