fd0ab286e3149ba3d36b9ab8d34376d0e0026b81
Diata Traore <dtraore@master.lct.jussieu.fr>
2020-10-12 15:12:13 +0200

    update test PBE

--- stable/test_func_der/energy_test_ecmd.irp.f
+++ stable/test_func_der/energy_test_ecmd.irp.f
@@ -1,118 +1,150 @@
-subroutine ecmdsrPBEn2_test(mu_in,rho_a,rho_b,grad_rho_a_2,grad_rho_b_2,grad_rho_a_b,rho2,ec_srmuPBE,decdrho_a,decdrho_b, decdrho, decdgrad_rho_a_2,decdgrad_rho_b_2,decdgrad_rho_a_b, decdgrad_rho_2,decdrho2)
+subroutine energy_cmd_supbeot_test (rho_a, rho_b, grad_rho_a, grad_rho_b, ec_supbeot_test)
+ implicit none
+ BEGIN_DOC
+! exchange energy with the lda functional
+ END_DOC
+ integer :: i,j,m
+ double precision, intent(in) :: rho_a(n_points_final_grid), rho_b(n_points_final_grid), grad_rho_a(3,n_points_final_grid), grad_rho_b(3,n_points_final_grid)
+ double precision, intent(out) :: ex_supbeot_test, ec_supbeot_test
+ double precision :: mu,weight
+ double precision :: ecmd_ot
+ double precision :: grad_rho_a_2,grad_rho_b_2,grad_rho_a_b
 
-  implicit none
-  BEGIN_DOC
-  ! Calculation of correlation energy and chemical potential in PBE approximation using multideterminantal wave function (short-range part) with exact on top pair density
-  ! The on-top pair density should be normalized to N(N-1)
-  END_DOC
+ ecmd_supbeot_test = 0.d0
+ mu = 999 !mu_of_r
 
-  double precision, intent(in)  :: mu_in
-  double precision, intent(in)  :: rho_a(n_points_final_grid),rho_b(n_points_final_grid),grad_rho_a_2(n_points_final_grid),grad_rho_b_2(n_points_final_grid),grad_rho_a_b(n_points_final_grid), rho2(n_points_final_grid)
-  double precision, intent(out) :: ec_srmuPBE,decdrho_a(n_points_final_grid),decdrho_b(n_points_final_grid),decdgrad_rho_a_2(n_points_final_grid),decdgrad_rho_b_2(n_points_final_grid),decdgrad_rho_a_b(n_points_final_grid), decdgrad_rho_2(n_points_final_grid), decdrho(n_points_final_grid), decdrho2(n_points_final_grid)!, decdrho2_a, decdrho2_b
-  double precision              :: rho(n_points_final_grid), m(n_points_final_grid)
-  double precision              :: mu
-  double precision              :: ecPBE,decPBEdrho_a,decPBEdrho_b,decPBEdrho, decPBEdgrad_rho_a_2,decPBEdgrad_rho_b_2,decPBEdgrad_rho_a_b
-  double precision              :: rho_c, rho_o,grad_rho_2,grad_rho_o_2,grad_rho_o_c,decPBEdrho_c,decPBEdrho_o,decPBEdgrad_rho_2,decPBEdgrad_rho_o_2, decPBEdgrad_rho_o
-  double precision              :: beta, dbetadrho, dbetadgrad_rho_2, denom, ddenomdrho, ddenomdgrad_rho_2, ddenomdrho2
-  double precision              :: pi, c, thr, weight
-  integer                       :: i
-  ec_srmuPBE       = 0.d0
-  decdrho_a        = 0.d0
-  decdrho_b        = 0.d0
-  decdrho          = 0.d0
-  decdgrad_rho_a_2 = 0.d0
-  decdgrad_rho_b_2 = 0.d0
-  decdgrad_rho_a_b = 0.d0
-  decdgrad_rho_2   = 0.d0
-  decdrho2         = 0.d0
-  
-   
   do i = 1, n_points_final_grid
-   rho(i) = rho_a(i) + rho_b(i)
-   if(rho(i).lt.1.d-10)then
-    return
-   else if(rho2(i)/(rho(i)**2) .lt. 1.d-6)then
-    return
-   endif
-   m(i) = rho_a(i) - rho_b(i)
-
-   pi = dacos(-1.d0)
-   thr = 1.d-12
-   mu = min(mu_in,1.d+10)
-
-! correlation PBE standard and on-top pair distribution 
-   call rho_ab_to_rho_oc(rho_a(i),rho_b(i),rho_o,rho_c)
-   call grad_rho_ab_to_grad_rho_oc(grad_rho_a_2(i),grad_rho_b_2(i),grad_rho_a_b(i),grad_rho_o_2,grad_rho_2,grad_rho_o_c)
-
-   call ec_pbe_sr(1.d-12,rho_c,rho_o,grad_rho_2,grad_rho_o_2,grad_rho_o_c,ecPBE,decPBEdrho_c,decPBEdrho_o,decPBEdgrad_rho_2,decPBEdgrad_rho_o_2, decPBEdgrad_rho_o)
-
-   call v_rho_oc_to_v_rho_ab(decPBEdrho_o, decPBEdrho_c, decPBEdrho_a, decPBEdrho_b)
-   call v_grad_rho_oc_to_v_grad_rho_ab(decPBEdgrad_rho_o_2, decPBEdgrad_rho_2, decPBEdgrad_rho_o, decPBEdgrad_rho_a_2, decPBEdgrad_rho_b_2, decPBEdgrad_rho_a_b)
-
-! calculation of energy
-   c = 2*dsqrt(pi)*(1.d0 - dsqrt(2.d0))/3.d0
-
-   if(dabs(rho2(i)).lt.1.d-10)then
-    beta = 1.d+10
-   else
-    beta = ecPBE/(c*rho2(i))
-   endif
-   if(dabs(beta).lt.thr)then
-    beta = 1.d-12
-   endif
-
-   denom = 1.d0 + beta*mu**3
    weight = final_weight_at_r_vector(i)
-   ec_srmuPBE += weight*ecPBE/denom
-   if(isnan(ec_srmuPBE))then
-    print*,'stop !!! isnan(ec_srmuPBE)'
-    print*,ecPBE,denom
-    print*,beta,mu
-    print*,rho_c,rho_o
-    print*,grad_rho_2,grad_rho_o_2,grad_rho_o_c
-    stop
-   endif
+   grad_rho_a_2 = 0.d0
+   grad_rho_b_2 = 0.d0
+   grad_rho_a_b = 0.d0
+   do m = 1, 3
+    grad_rho_a_2 += grad_rho_a(m,i) * grad_rho_a(m,i)
+    grad_rho_b_2 += grad_rho_b(m,i) * grad_rho_b(m,i)
+    grad_rho_a_b += grad_rho_a(m,i) * grad_rho_b(m,i)
+   enddo
 
-! calculation of derivatives 
-  !dec/dn
-  decPBEdrho = 0.5d0 *(decPBEdrho_a + decPBEdrho_b)
+   ! call ecmd_ot ...
 
-  if(dabs(rho2(i)).lt.1.d-10)then
-   dbetadrho = 1.d+10 ! - (ecPBE/(c*rho2**2))*dn2_UEGdrho
-  else
-   dbetadrho = decPBEdrho/(c*rho2(i)) ! - (ecPBE/(c*rho2**2))*dn2_UEGdrho
-  endif
-  ddenomdrho = dbetadrho*mu**3
-
-  decdrho(i) = decPBEdrho/denom - ecPBE*ddenomdrho/(denom**2)
-  decdrho_a(i) = decdrho(i)
-  decdrho_b(i) = decdrho(i)
-
-!dec/((dgradn)^2)
-  decPBEdgrad_rho_2 = 0.25d0 *(decPBEdgrad_rho_a_2 + decPBEdgrad_rho_b_2 + decPBEdgrad_rho_a_b)
-
-  if(dabs(rho2(i)).lt.1.d-10)then
-   dbetadgrad_rho_2 = 1.d+10
-  else
-   dbetadgrad_rho_2 = decPBEdgrad_rho_2/(c*rho2(i))
-  endif
-  ddenomdgrad_rho_2 = dbetadgrad_rho_2*mu**3
-
-  decdgrad_rho_2(i)   = decPBEdgrad_rho_2/denom - ecPBE*ddenomdgrad_rho_2/(denom**2)
-  decdgrad_rho_a_2(i) = decdgrad_rho_2(i) ! + decdgrad_n_m + decdgrad_m_2
-  decdgrad_rho_b_2(i) = decdgrad_rho_2(i) ! - decdgrad_n_m + decdgrad_m_2
-  decdgrad_rho_a_b(i) = 2.d0*decdgrad_rho_2(i) ! - 2.d0*decdgrad_m_2 
-
-  !dec/dn2
-
-  if(dabs(rho2(i)).gt.1.d-10)then
-   ddenomdrho2 = - (mu**3)* ecPBE/(c*rho2(i)**2)
-  else
-   ddenomdrho2 = - (mu**3)* ecPBE * 1.d-20
-  endif
-
-  decdrho2(i) = - ecPBE*ddenomdrho2/(denom**2)
-  ! decdrho2_a = decdrho2
-  ! decdrho2_b = decdrho2
+   ec_supbeot_test += ecmd_ot * weight
+ 
   enddo
-  end 
+end
+
+subroutine int_potential_c_supbeot_test (delta_rho_11, int_vc_supbeot_test)
+ implicit none
+ BEGIN_DOC
+! delta_Ex[n] = int_dr delta_n(r) dEx/dn(r) 
+! delta_n(r) <- subroutine delta_density_for_energy_test
+! d_e_d_n    <- subroutine de_dn_at_r
+ END_DOC
+ integer :: i
+ double precision, intent(in) :: delta_rho_11
+ double precision, intent(out):: int_vc_supbeot_test
+ double precision :: vc_i_j
+ double precision :: delta_gamma_i_j(mo_num, mo_num)
+ integer :: k,l
+ 
+ int_vc_supbeot_test = 0.d0
+ call delta_gamma_i_j_for_energy_test (delta_rho_11,delta_gamma_i_j)
+ do k=1, mo_num
+  do l=1, mo_num
+   call potential_c_supbeot(k,l, vc_i_j)
+   int_vc_supbeot_test += delta_gamma_i_j(k,l)*vc_i_j
+  enddo
+ enddo
+end
+
+subroutine int_potential_c_supbeot_n2_test (delta_rho_11, int_vc_supbeot_n2_test)
+ implicit none
+ BEGIN_DOC
+! delta_Ex[n] = int_dr delta_n(r) dEx/dn(r) 
+! delta_n(r) <- subroutine delta_density_for_energy_test
+! d_e_d_n    <- subroutine de_dn_at_r
+ END_DOC
+ integer :: i
+ double precision, intent(in) :: delta_rho_11
+ double precision, intent(out):: int_vc_supbeot_n2_test
+ double precision :: vc_i_j_n2
+ double precision :: delta_gamma_i_j(mo_num, mo_num)
+ integer :: j,k,l,m
+ 
+ int_vc_supbeot_n2_test = 0.d0
+ call delta_gamma_i_j_k_l_for_energy_test (delta_rho_11,delta_gamma_i_j_k_l)
+ do j=1, mo_num
+  do k=1, mo_num
+   do l=1, mo_num
+    do m=1, mo_num
+     call potential_c_supbeot(k,l,m,n,vc_i_j, vc_i_j_n2)
+     int_vc_supbeot_n2_test += delta_gamma_i_j(k,l)*vc_i_j_n2
+    enddo
+   enddo
+  enddo
+ enddo
+end
+
+
+
+subroutine potential_c_supbeot(k,l,vc_i_j, vc_i_j_n2)
+ implicit none
+ BEGIN_DOC
+! dEx/dn(r) = sum(k,l) {(vx_lda*mo_k*mo_l)}
+! vx_lda = vx_lda_alpha + vx_lda_beta
+ END_DOC
+ double precision, intent(out) :: vc_i_j, vc_i_j_n2
+ integer, intent(in) :: j,k,l,m
+ integer :: i
+ double precision  :: r(3), weight
+ double precision :: mo_j(mo_num), mo_k(mo_num) mo_l(mo_num), mo_m(mo_num)
+ vc_i_j = 0.d0
+ vc_i_j_n2 = 0.d0
+  
+ do i = 1, n_points_final_grid
+   r(1) = final_grid_points(1,i)   
+   r(2) = final_grid_points(2,i) 
+   r(3) = final_grid_points(3,i)
+   weight = final_weight_at_r_vector(i)
+
+   call give_all_mos_at_r(r, mo_j)
+   call give_all_mos_at_r(r, mo_k)
+   call give_all_mos_at_r(r, mo_l)
+   call give_all_mos_at_r(r, mo_m)
+
+   vc_i_j += weight*(potential_c_alpha_mo_supbeot(k,l) + potential_c_beta_mo_supbeot(k,l)) * mo_k(k) * mo_l(l)
+   vc_i_j_n2 += weight*(potential_c_alpha_mo_supbeot_n2(j,k,l,m) + potential_c_beta_mo_supbeot_n2(j,k,l,m)) * mo_j(j) * mo_k(k)* mo_l(l) * mo_m(m)
+ enddo
+end
+ 
+BEGIN_PROVIDER [double precision, potential_c_alpha_mo_supbeot, (mo_num,mo_num)]
+ implicit none
+ BEGIN_DOC
+ !
+ END_DOC
+! call ao_to_mo(potential_c_alpha_ao_supbeot,ao_num,potential_c_alpha_mo_supbeot,mo_num)
+ potential_c_alpha_mo_supbeot(
+END_PROVIDER 
+
+BEGIN_PROVIDER [double precision, potential_c_beta_mo_supbeot, (mo_num,mo_num)]
+ implicit none
+ BEGIN_DOC
+ !
+ END_DOC
+ call ao_to_mo(potential_c_beta_ao_supbeot,ao_num,potential_c_beta_mo_supbeot,mo_num)
+END_PROVIDER
+
+BEGIN_PROVIDER [double precision, potential_c_alpha_mo_supbeot_n2, (mo_num,mo_num)]
+ implicit none
+ BEGIN_DOC
+ !
+ END_DOC
+ call ao_to_mo(potential_c_alpha_ao_supbeot,ao_num,potential_c_alpha_mo_supbeot,mo_num)
+END_PROVIDER 
+
+BEGIN_PROVIDER [double precision, potential_c_alpha_mo_supbeot_n2, (mo_num,mo_num)]
+ implicit none
+ BEGIN_DOC
+ !
+ END_DOC
+ call ao_to_mo(potential_c_alpha_ao_supbeot,ao_num,potential_c_alpha_mo_supbeot,mo_num)
+END_PROVIDER 
+
--- stable/test_func_der/test_energy_intergral_lda.irp.f
+++ stable/test_func_der/test_energy_intergral_lda.irp.f
@@ -29,6 +29,7 @@ program test_energy_integral_lda
  double precision :: delta_dm_a(n_points_final_grid), delta_dm_b(n_points_final_grid), dm_a_plus_delta(n_points_final_grid), dm_b_plus_delta(n_points_final_grid)
  double precision :: ex_lda_test, ex_lda_test_plus_delta, int_vx_lda_test
  double precision :: ex_pbe_test, ex_pbe_test_plus_delta, int_vx_pbe_test
+ double precision :: ec_pbe_test, ec_pbe_test_plus_delta, int_vc_pbe_test
  double precision :: pi
  double precision :: delta_rho_11, integral_potential_lda, ex_lda_potential, weight
  integer :: i, istate
@@ -59,11 +60,12 @@ program test_energy_integral_lda
    call energy_x_lda_test(dm_a_plus_delta, dm_b_plus_delta, ex_lda_test_plus_delta)
    call int_potential_x_lda_test (delta_rho_11, int_vx_lda_test) 
 
-   !exPBE
-   call energy_x_pbe_test (dm_a, dm_b, grad_dm_a, grad_dm_b, ex_pbe_test)
-   call energy_x_pbe_test (dm_a_plus_delta, dm_b_plus_delta, grad_dm_a, grad_dm_b, ex_pbe_test_plus_delta)
-   call int_potential_x_pbe_test (delta_rho_11, int_vx_pbe_test)
- 
+   !excPBE
+   call energy_xc_pbe_test (dm_a, dm_b, grad_dm_a, grad_dm_b, ex_pbe_test, ec_pbe_test)
+   call energy_xc_pbe_test (dm_a_plus_delta, dm_b_plus_delta, grad_dm_a, grad_dm_b, ex_pbe_test_plus_delta, ec_pbe_test_plus_delta)
+   call int_potential_xc_pbe_test (delta_rho_11, int_vx_pbe_test, int_vc_pbe_test)
+
+
    write(34,*) 'gamma_i_j = epsilon*delta_kronecker(i,1)*delta_kronecker(1,j) with epsilon =', delta_rho_11
    write(34,*) '----------------------------------------------LDA--------------------------------------------' 
    write(34,*) 'ex_lda_test[n]                                                          =', ex_lda_test
@@ -82,5 +84,9 @@ program test_energy_integral_lda
    write(34,*) '2nd method = sum(i,j) delta_gamma_i_j * int (dr phi_i(r) v(r) phi_j(r)) =', int_vx_pbe_test
    write(34,*) 'Relative error : (1st_method - 2nd_method)/1st_method                   =', (int_vx_pbe_test - ex_pbe_test_plus_delta + ex_pbe_test)/(ex_pbe_test_plus_delta - ex_pbe_test)
 
+   write(34,*) '------------------------------------------------'
+   write(34,*) '1st_method = ec_pbe [n + delta_n] - ex_pbe[n]                           =', ec_pbe_test_plus_delta - ec_pbe_test
+   write(34,*) '2nd method = sum(i,j) delta_gamma_i_j * int (dr phi_i(r) v(r) phi_j(r)) =', int_vc_pbe_test
+   write(34,*) 'Relative error : (1st_method - 2nd_method)/1st_method                   =', (int_vc_pbe_test - ec_pbe_test_plus_delta + ec_pbe_test)/(ec_pbe_test_plus_delta - ec_pbe_test)
 end program
 
