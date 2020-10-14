162332dc2aa081a74e8b03e175b8cd2c7766f199
Emmanuel Giner <giner.emmanuel@gmail.com>
2020-10-14 11:34:44 +0200

    re added func_ecmd

--- dev/slat_rules_trans/test_density.irp.f
+++ dev/slat_rules_trans/test_density.irp.f
@@ -26,7 +26,13 @@ subroutine routine_write
  call routine_exact(on_top_exact,npts,xmax)
  dx = xmax/dble(npts)
  x = 0.d0
+ double precision :: accu1, accu2, accu3, mu_lda, mu_exact
+ accu1 = 0.d0
+ accu2 = 0.d0
+ accu3 = 0.d0
  do i = 1, npts
+  mu_lda   =  - 1.d0 / (dlog(on_top_g0(i)/on_top_hf(i)) * sqpi) 
+  mu_exact =  - 1.d0 / (dlog(on_top_exact(i)/on_top_hf(i)) * sqpi)
   !                          1     2             3             4                5            
   write(33,'(100(F16.10,X))')x,on_top_psi(i),on_top_hf(i), on_top_g0(i), on_top_exact(i), &  
                              !                    6 
@@ -34,9 +40,17 @@ subroutine routine_write
                              !  7              8                                        9              10
                              g0_array(i), 2.d0 * on_top_exact(i)/ dm_array(i)**2.d0, dm_hf_array(i),dm_array(i), &
                              !     11                                               12
-                             - 1.d0 * sqpi / dlog(on_top_exact(i)/on_top_psi(i)), mu_hf(i)
+                             - 1.d0 / (dlog(on_top_exact(i)/on_top_psi(i))* sqpi) , mu_hf(i), &
+                             !     13   14 
+                             mu_exact, mu_lda
   x += dx
+  accu1 += dm_hf_array(i) * dx 
+  accu2 += dm_hf_array(i) * dx * mu_lda
+  accu3 += dm_hf_array(i) * dx * mu_exact
  enddo
+ print*,'mu_exact = ',accu3/accu1
+ print*,'mu_lda   = ',accu2/accu1
+ print*,'accu1    = ',accu1
 end
 
 subroutine routine_psi_hf(dm_hf_array,dm_array,g0_array,on_top_psi,on_top_hf,on_top_g0, mu_hf,  npts,xmax)
@@ -100,3 +114,30 @@ subroutine routine_exact(on_top_exact,npts,xmax)
   r(1) += dx
  enddo
 end
+
+ BEGIN_PROVIDER [double precision, average_mu_lda_exact]
+&BEGIN_PROVIDER [double precision, average_mu_lda      ]
+ implicit none
+ integer :: ipoint,i
+ double precision :: weight, rho_a_hf, rho_b_hf, on_top_hf, g0, on_top_g0
+ double precision :: rho_tot_hf, g0_UEG_mu_inf
+ average_mu_lda_exact = 0.d0
+ average_mu_lda       = 0.d0
+ do ipoint = 1, n_points_final_grid
+  weight = final_weight_at_r_vector(ipoint)
+  rho_a_hf = 0.d0
+  do i = 1, elec_alpha_num 
+   rho_a_hf += mos_in_r_array(i,ipoint)*mos_in_r_array(i,ipoint)
+  enddo
+  rho_b_hf = 0.d0
+  do i = 1, elec_beta_num 
+   rho_b_hf += mos_in_r_array(i,ipoint)*mos_in_r_array(i,ipoint)
+  enddo
+  rho_tot_hf = rho_b_hf + rho_a_hf
+  on_top_hf = rho_a_hf * rho_b_hf 
+  g0 = g0_UEG_mu_inf(rho_a_hf,rho_b_hf)
+  on_top_g0 = g0 * 0.5d0 * rho_tot_hf*rho_tot_hf
+!  - 1.d0 / (dlog(on_top_exact(i)/on_top_hf(i)) * sqpi)
+ enddo 
+
+END_PROVIDER 
