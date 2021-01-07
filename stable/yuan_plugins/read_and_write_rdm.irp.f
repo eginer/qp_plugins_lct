b714e8e61148f43a6716ce5238d0180fde4beac5
Emmanuel Giner <giner.emmanuel@gmail.com>
2021-01-07 14:38:01 +0100

    changed the read two_rdm in yuan_plugins

--- stable/yuan_plugins/read_and_write_rdm.irp.f
+++ stable/yuan_plugins/read_and_write_rdm.irp.f
@@ -3,10 +3,14 @@ subroutine read_two_rdm_and_write_to_ezfio(n,n_mo_tmp)
  implicit none
  integer, intent(in) :: n,n_mo_tmp
 
- double precision, allocatable :: two_rdm(:,:,:,:)
- allocate(two_rdm(n_mo_tmp,n_mo_tmp,n_mo_tmp,n_mo_tmp))
  integer :: i,j,k,l,m
  double precision :: value_rdm
+ double precision, allocatable :: two_rdm(:,:,:,:,:)
+ character*(128) :: name_file 
+ integer :: nstates
+ nstates = 1
+
+ allocate(two_rdm(n_mo_tmp,n_mo_tmp,n_mo_tmp,n_mo_tmp,nstates))
  character*(1) :: coma
  open(1, file = 'two_rdm') 
  two_rdm = 0.d0
@@ -14,16 +18,18 @@ subroutine read_two_rdm_and_write_to_ezfio(n,n_mo_tmp)
    ! a^{l}a^{k} a_i a_j
 !  read(1,'(4(I3,A1),F16.13)')l,coma, k, coma, j, coma, i,coma,value_rdm
   read(1,*)l, k,  j,  i,value_rdm
-  two_rdm(l,k,j,i) = 0.5d0 * value_rdm
+  two_rdm(l,k,j,i,1) = 0.5d0 * value_rdm
  enddo
  close(1)
 
+ name_file = 'act_2_rdm_ab_mo'
  ! Writting the two rdm on the alpha/beta
- call ezfio_set_two_body_rdm_two_rdm_ab_disk(two_rdm)
+ call write_array_two_rdm(n_mo_tmp,nstates,two_rdm,name_file)
  call ezfio_set_two_body_rdm_io_two_body_rdm_ab("Read")
 
  ! Writting the two rdm on the spin trace
- call ezfio_set_two_body_rdm_two_rdm_spin_trace_disk(two_rdm)
+ name_file = 'act_2_rdm_spin_trace_mo'
+ call write_array_two_rdm(n_mo_tmp,nstates,two_rdm,name_file)
  call ezfio_set_two_body_rdm_io_two_body_rdm_spin_trace("Read")
 
  deallocate(two_rdm)
