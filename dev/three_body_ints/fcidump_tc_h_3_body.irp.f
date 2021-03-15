43e8d7b28291ddff467b73f2c1ca3d7d8dbd6df3
Emmanuel Giner <giner.emmanuel@gmail.com>
2021-03-15 22:56:22 +0100

    two and three body tc fcidump in one exec

--- dev/three_body_ints/fcidump_tc_h_3_body.irp.f
+++ dev/three_body_ints/fcidump_tc_h_3_body.irp.f
@@ -3,12 +3,11 @@ program fcidump_tc_h_3_body
 ! call test_mo_erf
  integer :: i,j,k,l,m,n
  double precision :: integral 
- character*(128) :: output_chemist, output_physicist
- integer :: i_unit_output_chemist, i_unit_output_physicist
+ character*(128) :: output_physicist
+ integer :: i_unit_output_physicist
  integer :: getUnitAndOpen
-! output_chemist =trim(ezfio_filename)//'.FCIDUMP_3_body_chemist'
-! i_unit_output_chemist = getUnitAndOpen(output_chemist,'w')
- output_physicist =trim(ezfio_filename)//'.FCIDUMP_3_body_physicist'
+ call two_body_fcidum_tc
+ output_physicist =trim(ezfio_filename)//'.FCIDUMP_3_body_tc'
  i_unit_output_physicist = getUnitAndOpen(output_physicist,'w')
  if(read_six_index_tensor)then
   provide three_body_ints
@@ -54,4 +53,85 @@ program fcidump_tc_h_3_body
   enddo
  endif
 
+end
+
+subroutine two_body_fcidum_tc
+  implicit none
+  BEGIN_DOC
+! Produce a regular `FCIDUMP` file from the |MOs| stored in the |EZFIO|
+! directory.
+!
+! To specify an active space, the class of the |MOs| have to set in the
+! |EZFIO| directory (see :ref:`qp_set_mo_class`).
+!
+! The :ref:`fcidump` program supports 3 types of |MO| classes :
+!
+! * the *core* orbitals which are always doubly occupied in the
+!   calculation
+!
+! * the *deleted* orbitals that are never occupied in the calculation
+!
+! * the *active* orbitals that are occupied with a varying number of
+!   electrons
+!
+  END_DOC
+  character*(128) :: output
+  integer :: i_unit_output,getUnitAndOpen
+  output=trim(ezfio_filename)//'.FCIDUMP_2_body_tc'
+  i_unit_output = getUnitAndOpen(output,'w')
+
+  integer :: i,j,k,l
+  integer :: i1,j1,k1,l1
+  integer :: i2,j2,k2,l2
+  integer*8 :: m
+  character*(2), allocatable :: A(:)
+
+  write(i_unit_output,*) '&FCI NORB=', n_act_orb, ', NELEC=', elec_num-n_core_orb*2, &
+   ', MS2=', (elec_alpha_num-elec_beta_num), ','
+  allocate (A(n_act_orb))
+  A = '1,'
+  write(i_unit_output,*) 'ORBSYM=', (A(i), i=1,n_act_orb)
+  write(i_unit_output,*) 'ISYM=0,'
+  write(i_unit_output,*) '/'
+  deallocate(A)
+
+  integer(key_kind), allocatable :: keys(:)
+  double precision, allocatable  :: values(:)
+  integer(cache_map_size_kind)   :: n_elements, n_elements_max
+  PROVIDE mo_two_e_integrals_erf_in_map mo_two_e_eff_dr12_pot_array_physicist mo_two_e_integrals_eff_pot_in_map
+
+  double precision :: get_mo_two_e_integral_erf, integral,mo_two_e_integral_eff_pot
+
+  do l=1,n_act_orb
+   l1 = list_act(l)
+   do k=1,n_act_orb
+    k1 = list_act(k)
+    do j=1,n_act_orb
+     j1 = list_act(j)
+     do i=1,n_act_orb
+      i1 = list_act(i)
+!       if (i1>=j1) then
+          integral = get_mo_two_e_integral_erf(i1,j1,k1,l1,mo_integrals_erf_map)
+          integral += mo_two_e_integral_eff_pot(i1,j1,k1,l1)
+          integral += mo_two_e_eff_dr12_pot_array_physicist(i1,j1,k1,l1)
+          if (dabs(integral) > mo_integrals_threshold) then
+            write(i_unit_output,*) integral, i,k,j,l
+          endif
+!       end if
+     enddo
+    enddo
+   enddo
+  enddo
+
+  do j=1,n_act_orb
+   j1 = list_act(j)
+   do i=1,n_act_orb
+    i1 = list_act(i)
+      integral = mo_one_e_integrals(i1,j1) + core_fock_operator(i1,j1)
+      if (dabs(integral) > mo_integrals_threshold) then
+        write(i_unit_output,*) integral, i,j,0,0
+      endif
+   enddo
+  enddo
+  write(i_unit_output,*) core_energy, 0, 0, 0, 0
 end
