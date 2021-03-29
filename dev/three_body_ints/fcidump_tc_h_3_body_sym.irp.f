50838df18fbea7550a457c9c80a89f8316de3ac1
Emmanuel Giner <giner.emmanuel@gmail.com>
2021-03-29 20:03:22 +0200

    modified three_body fcidump

--- dev/three_body_ints/fcidump_tc_h_3_body_sym.irp.f
+++ dev/three_body_ints/fcidump_tc_h_3_body_sym.irp.f
@@ -11,23 +11,31 @@ end
 subroutine fcidump_3_tc
  implicit none
  integer :: i,j,k,l,m,n
+ integer :: ii,jj,kk,ll,mm,nn
  double precision :: integral 
  character*(128) :: output_physicist
  integer :: i_unit_output_physicist
  integer :: getUnitAndOpen
  output_physicist =trim(ezfio_filename)//'/FCIDUMP_3_body_tc_sym'
  i_unit_output_physicist = getUnitAndOpen(output_physicist,'w')
-  do n = 1, mo_num
-   do l = 1, mo_num
-    do k = 1, mo_num
-     do m = n, mo_num
-      do j = l, mo_num
-       do i = k, mo_num
+  do nn = 1, n_act_orb
+   n = list_act(nn)
+   do ll = 1, n_act_orb
+    l = list_act(ll)
+    do kk = 1, n_act_orb
+     k = list_act(kk)
+     do mm = n, n_act_orb
+      m = list_act(mm)
+      do jj = l, n_act_orb
+       j = list_act(jj)
+       do ii = k, n_act_orb
+        i = list_act(ii)
         !                          1 2 3 1 2 3
         !                         <i j m|k l n>
         !                         (ik|jl|mn)
-        integral = three_body_ints(i,j,m,k,l,n)
-        integral = integral * 1.d0/3.d0 !!!! For NECI convention 
+!        integral = three_body_ints(i,j,m,k,l,n)
+        call give_integrals_3_body(i,j,m,k,l,n,integral)
+        integral = -integral * 1.d0/3.d0 !!!! For NECI convention 
         if(dabs(integral).lt.1.d-12)cycle
         write(i_unit_output_physicist,'(E20.10,6(I3,X))') integral, i, j, m, k, l, n
         write(i_unit_output_physicist,'(E20.10,6(I3,X))') integral, k,j,m,i,l,n
