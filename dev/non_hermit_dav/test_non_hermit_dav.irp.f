3ca3d5646a720ff27c6e176c5e2728317f8a6711
Emmanuel Giner <giner.emmanuel@gmail.com>
2021-03-30 16:35:52 +0200

    added generalized non symmetric eigenvalue problem

--- dev/non_hermit_dav/test_non_hermit_dav.irp.f
+++ dev/non_hermit_dav/test_non_hermit_dav.irp.f
@@ -1,5 +1,10 @@
 program pouet
  implicit none
+ call test_general_lapack
+end
+
+subroutine test_dav
+ implicit none
  integer :: sze,nstates
  double precision, allocatable :: reigv(:,:),leigv(:,:),eigval(:)
  external hcalc_r_tmp
@@ -20,4 +25,79 @@ program pouet
   write(*,'(I4,X,2(F16.10,X))')i,reigv(i,1),reigvec_ht(i,1)
  enddo
 
+end
+
+subroutine test_general_lapack
+ implicit none
+ double precision, allocatable :: B(:,:),A(:,:)
+ double precision, allocatable :: reigvec(:,:),leigvec(:,:),eigval(:)
+ double precision, allocatable :: non_ortho_basis(:,:)
+ integer :: n_real_eigv,sze,i,j,k,l
+ sze = 10
+ allocate(B(sze,sze),A(sze,sze),non_ortho_basis(sze,sze))
+ allocate(reigvec(sze,sze),leigvec(sze,sze),eigval(sze))
+ A = 0.d0
+ B = 0.d0
+ do i = 1, sze
+  B(i,i) = 1.d0
+  do j = 1, sze
+   A(j,i) = h_non_hermit(j,i)
+  enddo
+ enddo
+ print*,'With the generalized eigenvalue problem matrix '
+ call non_hrmt_general_real_diag(sze,A,B,reigvec,leigvec,n_real_eigv,eigval)
+ print*,''
+ do i = 1, n_real_eigv
+  print*,'i ',i,eigval(i)
+ enddo
+
+ print*,'With the usual eigenvalue problem matrix '
+ call non_hrmt_real_diag(sze,A,reigvec,leigvec,n_real_eigv,eigval)
+ print*,''
+ do i = 1, n_real_eigv
+  print*,'i ',i,eigval(i)
+ enddo
+
+ print*,''
+ print*,''
+ print*,'Non orthonormal basis'
+ print*,''
+ non_ortho_basis = 0.d0
+ do i = 1, sze
+  do j = 1, sze
+    if(i==j)then
+     non_ortho_basis(j,i) = 1.d0
+    else
+     non_ortho_basis(j,i) = 1.d0/dabs(dble(i-j))
+    endif
+  enddo
+ enddo
+ A = 0.d0
+ B = 0.d0
+ do i = 1, sze
+  do j = 1, sze
+   do k = 1, sze
+    B(j,i) += non_ortho_basis(k,i) * non_ortho_basis(k,j)
+    do l = 1, sze
+     A(j,i) += non_ortho_basis(k,i) * non_ortho_basis(l,k) * h_non_hermit(l,j)
+    enddo
+   enddo
+  enddo
+ enddo
+
+ print*,'With the generalized eigenvalue problem matrix '
+ print*,''
+ print*,''
+ print*,'Overlap matrix'
+ print*,''
+ do i = 1, sze
+  write(*,'(100(F16.10,X))')B(i,:)
+ enddo
+ print*,''
+ print*,''
+ call non_hrmt_general_real_diag(sze,A,B,reigvec,leigvec,n_real_eigv,eigval)
+ print*,''
+ do i = 1, n_real_eigv
+  print*,'i ',i,eigval(i)
+ enddo
 end
