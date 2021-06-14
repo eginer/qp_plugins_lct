f4beca975fa057757009bfe38c8605450e750cbe
Emmanuel Giner <giner.emmanuel@gmail.com>
2021-06-14 20:14:29 +0200

    mu(r) rsc compiles

--- dev/one_e_ints_mu_of_r/mu_rsc.irp.f
+++ dev/one_e_ints_mu_of_r/mu_rsc.irp.f
@@ -5,90 +5,44 @@
  implicit none
  integer :: ipoint,i,m
  include 'constants.include.F'
- double precision :: weight, rho_a_hf, rho_b_hf, rho_hf, mu_rs_c
+ double precision :: weight, rho_a_hf, rho_b_hf, rho_hf, mu_rs_c,r(3)
  average_mu_rs_c    = 0.d0
- double precision :: elec_a,elec_b
- double precision :: mu_plus, mu_minus, r(3),dx,mu_lda
+ double precision :: elec_a,elec_b,grad_mu(3)
+ double precision :: dx,mu_lda, mu_tmp,mu_min,mu,damped_mu
  dx = 1.d-5
  elec_a = 0.d0
  elec_b = 0.d0
+ mu_min = mu_erf
  do ipoint = 1, n_points_final_grid
   weight = final_weight_at_r_vector(ipoint)
+  r(:) = final_grid_points_extra(:,ipoint)
   rho_a_hf = one_e_dm_and_grad_alpha_in_r(4,ipoint,1)
   rho_b_hf = one_e_dm_and_grad_beta_in_r(4,ipoint,1)
   rho_hf = rho_a_hf + rho_b_hf
-  mu_of_r_rs_c(ipoint,1) = mu_rs_c(rho_hf) 
+  mu_tmp = mu_rs_c(rho_hf)
+  if(damped_mu_of_r)then
+   mu = damped_mu(mu_tmp,mu_min)
+  else
+   mu = max(mu_tmp,mu_of_r_min)
+  endif
+  mu_of_r_rs_c(ipoint,1) = mu
   average_mu_rs_c   +=  mu_of_r_rs_c(ipoint,1) * rho_hf * weight
   elec_a += rho_a_hf * weight
   elec_b += rho_b_hf * weight
 
-  do m = 1, 3
-   r(:) = final_grid_points(:,ipoint) 
-   r(m) += dx 
-   call dm_dft_alpha_beta_at_r(r,rho_a_hf,rho_b_hf)
-   rho_hf = rho_a_hf + rho_b_hf
-   mu_plus = mu_rs_c(rho_hf)
-   r(:) = final_grid_points(:,ipoint) 
-   r(m) -= dx 
-   call dm_dft_alpha_beta_at_r(r,rho_a_hf,rho_b_hf)
-   rho_hf = rho_a_hf + rho_b_hf
-   mu_minus = mu_rs_c(rho_hf)
-
-   grad_mu_of_r_rs_c(m,ipoint,1) = (mu_plus - mu_minus)/(2.d0 * dx)
-  enddo
+  if(damped_mu_of_r)then
+   call get_grad_damped_mu_rsc(r,mu_min,dx,grad_mu) 
+  else
+   call get_grad_mu_rsc(r,dx,grad_mu)
+  endif
+  grad_mu_of_r_rs_c(:,ipoint,1) = grad_mu(:)
 
  enddo 
  average_mu_rs_c   = average_mu_rs_c  / dble(elec_a+ elec_b)
 
 END_PROVIDER 
 
-BEGIN_PROVIDER [ double precision, average_mu_rs_c_lda]
- implicit none
- average_mu_rs_c_lda = 0.5d0 * (average_mu_rs_c + average_mu_lda)
-END_PROVIDER 
 
- BEGIN_PROVIDER [double precision, average_mu_grad_n   ]
- implicit none
- integer :: ipoint,i,m
- double precision :: sqpi
- double precision :: weight, rho_a_hf, rho_b_hf, g0,rho_hf
- double precision :: rs,grad_n
- double precision :: g0_UEG_mu_inf
- double precision :: cst_rs,alpha_rs
- sqpi = dsqrt(dacos(-1.d0))
- cst_rs   = (4.d0 * dacos(-1.d0)/3.d0)**(-1.d0/3.d0)
- alpha_rs = 2.d0 * dsqrt((9.d0 * dacos(-1.d0)/4.d0)**(-1.d0/3.d0)) / sqpi
- average_mu_grad_n  = 0.d0 
- double precision :: elec_a,elec_b
- elec_a = 0.d0
- elec_b = 0.d0
- do ipoint = 1, n_points_final_grid
-  weight = final_weight_at_r_vector(ipoint)
-  rho_a_hf = 0.d0
-  grad_n   = 0.d0
-  rho_a_hf = one_e_dm_and_grad_alpha_in_r(4,ipoint,1)
-  rho_b_hf = one_e_dm_and_grad_beta_in_r(4,ipoint,1)
-  grad_n = one_e_grad_2_dm_alpha_at_r(ipoint,1) + one_e_grad_2_dm_beta_at_r(ipoint,1)
-  grad_n += 2.d0 * scal_prod_grad_one_e_dm_ab(ipoint,1)
-  rho_hf = rho_a_hf + rho_b_hf
-  grad_n = dsqrt(grad_n)
-  if(dabs(rho_hf).gt.1.d-20)then
-   grad_n = grad_n/(4.d0 * rho_hf)
-  else
-   grad_n = 0.d0
-  endif
-  rs = cst_rs * rho_hf**(-1.d0/3.d0)
-  g0 = g0_UEG_mu_inf(rho_a_hf,rho_b_hf)
-
-  average_mu_grad_n += grad_n * rho_hf * weight
-  elec_a += rho_a_hf * weight
-  elec_b += rho_b_hf * weight
- enddo 
- print*,'elec_a,elec_b',elec_a,elec_b
- average_mu_grad_n = average_mu_grad_n/ dble(elec_a+ elec_b)
-
-
-END_PROVIDER 
 
 double precision function mu_rs_c(rho)
  implicit none
@@ -103,6 +57,61 @@ double precision function mu_rs_c(rho)
 
 end
 
+double precision function damped_mu_rs_c(rho,mu_min)
+ implicit none
+ double precision, intent(in) :: rho,mu_min
+ double precision :: mu_rs_c,mu_tmp,damped_mu
+ mu_tmp = mu_rs_c(rho)
+ damped_mu_rs_c = damped_mu(mu_tmp, mu_min)
+end
+
+subroutine get_grad_mu_rsc(r,dx,grad_mu)
+ implicit none
+ double precision, intent(in) :: r(3),dx
+ double precision, intent(out):: grad_mu(3)
+ integer :: m
+ double precision :: r_tmp(3),mu_plus, mu_minus,mu_rs_c,rho_a_hf,rho_b_hf,rho_hf
+  do m = 1, 3
+   r_tmp = r 
+   r_tmp(m) += dx 
+   call dm_dft_alpha_beta_at_r(r_tmp,rho_a_hf,rho_b_hf)
+   rho_hf = rho_a_hf + rho_b_hf
+   mu_plus = mu_rs_c(rho_hf)
+   r_tmp = r 
+   r_tmp(m) -= dx 
+   call dm_dft_alpha_beta_at_r(r_tmp,rho_a_hf,rho_b_hf)
+   rho_hf = rho_a_hf + rho_b_hf
+   mu_minus = mu_rs_c(rho_hf)
+
+   grad_mu(m) = (mu_plus - mu_minus)/(2.d0 * dx)
+  enddo
+end
+
+subroutine get_grad_damped_mu_rsc(r,mu_min,dx,grad_mu)
+ implicit none
+ double precision, intent(in) :: r(3),mu_min,dx
+ double precision, intent(out):: grad_mu(3)
+ integer :: m
+ double precision :: r_tmp(3),mu_plus, mu_minus,mu_rs_c
+ double precision :: rho_a_hf,rho_b_hf,rho_hf,damped_mu,rho,damped_mu_rs_c
+  do m = 1, 3
+   r_tmp = r 
+   r_tmp(m) += dx 
+   call dm_dft_alpha_beta_at_r(r_tmp,rho_a_hf,rho_b_hf)
+   rho_hf = rho_a_hf + rho_b_hf
+   mu_plus = damped_mu_rs_c(rho,mu_min)
+
+   r_tmp = r 
+   r_tmp(m) -= dx 
+   call dm_dft_alpha_beta_at_r(r_tmp,rho_a_hf,rho_b_hf)
+   rho_hf = rho_a_hf + rho_b_hf
+   mu_minus = damped_mu_rs_c(rho,mu_min)
+
+   grad_mu(m) = (mu_plus - mu_minus)/(2.d0 * dx)
+  enddo
+end
+
+
  BEGIN_PROVIDER [double precision, mu_of_r_extra_grid_rs_c, (n_points_extra_final_grid,N_states) ]
  implicit none
  integer :: ipoint,i,m
@@ -110,16 +119,23 @@ end
  double precision :: weight, rho_a_hf, rho_b_hf, rho_hf, mu_rs_c
  average_mu_rs_c    = 0.d0
  double precision :: elec_a,elec_b,r(3)
- double precision :: mu_plus, mu_minus, dx,mu_lda
+ double precision :: mu_tmp ,damped_mu, mu ,dx,mu_min
  dx = 1.d-5
  elec_a = 0.d0
  elec_b = 0.d0
+ mu_min = mu_erf 
  do ipoint = 1, n_points_extra_final_grid
   weight = final_weight_at_r_vector_extra(ipoint)
   r(:) = final_grid_points_extra(:,ipoint)
   call dm_dft_alpha_beta_at_r(r,rho_a_hf,rho_b_hf)
   rho_hf = rho_a_hf + rho_b_hf
-  mu_of_r_extra_grid_rs_c(ipoint,1) = mu_rs_c(rho_hf) 
+  mu_tmp = mu_rs_c(rho_hf)
+  if(damped_mu_of_r)then
+   mu = damped_mu(mu_tmp,mu_min)
+  else
+   mu = max(mu_tmp,mu_of_r_min)
+  endif
+  mu_of_r_extra_grid_rs_c(ipoint,1) = mu
   elec_a += rho_a_hf * weight
   elec_b += rho_b_hf * weight
  enddo 
