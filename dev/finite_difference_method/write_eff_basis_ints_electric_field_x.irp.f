ac31e1175a4a1e82af6f9af080dcc2510854361f
Diata Traore <dtraore@master.lct.jussieu.fr>
2021-07-01 16:11:51 +0200

    update dev irp files

---
+++ dev/finite_difference_method/write_eff_basis_ints_electric_field_x.irp.f
@@ -0,0 +1,31 @@
+program write_effective_x_electric_field_hamiltonian
+ implicit none
+ BEGIN_DOC
+ !
+ ! The next programs that will run unto the EZFIO folder will, by default, 
+ !
+ ! have the one- and two-body integrals loaded from the EZFIO data. 
+ END_DOC
+ ! total one-e integrals 
+ io_mo_one_e_integrals = "None"
+ touch io_mo_one_e_integrals  
+ ! Vne integrals on the MO basis 
+ io_mo_integrals_n_e = "None"
+ touch io_mo_integrals_n_e
+ ! kinetic integrals on the MO basis 
+ io_mo_integrals_kinetic = "None"
+ touch io_mo_integrals_kinetic 
+ ! Vne integrals on the AO basis 
+ io_ao_integrals_n_e = "None"
+ touch io_ao_integrals_n_e 
+ ! kinetic integrals on the AO basis 
+ io_ao_integrals_kinetic = "None"
+ touch io_ao_integrals_kinetic 
+ io_ao_one_e_integrals = "None"
+ touch io_ao_one_e_integrals  
+
+ call save_v_ne_and_x_electric_filed_ao_ints
+end
+
+
+
---
+++ dev/pot_ecmd/print_tot_dipole.irp.f
@@ -0,0 +1,7 @@
+program print_tot_dipole
+ implicit none
+ read_wf = .True.
+ touch read_wf 
+ call print_tot_dipole_moment_only
+
+end
