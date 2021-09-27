79f3fbdd610493d2ea799221bb820da955ce8a94
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2021-09-27 16:33:48 +0200

    OMP collapse

--- dev/transcorr_h/get_delta.irp.f
+++ dev/transcorr_h/get_delta.irp.f
@@ -9,14 +9,26 @@ subroutine get_delta_no_store(psidet,psicoef,ndet,delta)
  double precision, intent(out)  :: delta(ndet) 
  double precision :: hij,htilde_ij,delta_mat,hmono,heff,hderiv,hthree
  integer :: i,j
+ PROVIDE scalar_mu_r_pot_physicist_mo deriv_mu_r_pot_physicist_mo
+ PROVIDE three_body_3_index three_body_3_index_exch_12 three_body_3_index_exch_13 three_body_3_index_exch_23
+ PROVIDE three_body_5_index three_body_5_index_exch_13 three_body_5_index_exch_32
+ PROVIDE three_body_4_index three_body_4_index_exch_12 three_body_4_index_exch_12_part
+
  delta = 0.d0
-  do j = 1, N_det
-   do i = 1, N_det
+ i=1
+ j=1
+ call htilde_mat(psi_det(1,1,i),psi_det(1,1,j),hmono,heff,hderiv,hthree,htilde_ij)
+ call  i_H_j(psidet(1,1,i),psidet(1,1,j),N_int,hij)
+ !$OMP PARALLEL DO DEFAULT(SHARED) SCHEDULE(dynamic) &
+ !$OMP PRIVATE(i,j,delta_mat,hmono,heff,hderiv,hthree,htilde_ij,hij)
+  do i = 1, N_det
+   do j = 1, N_det
     call htilde_mat(psi_det(1,1,i),psi_det(1,1,j),hmono,heff,hderiv,hthree,htilde_ij)
     call  i_H_j(psidet(1,1,i),psidet(1,1,j),N_int,hij)
     delta_mat = htilde_ij - hij 
-    delta(i) += psicoef(j) * delta_mat
+    delta(i) = delta(i) + psicoef(j) * delta_mat
    enddo
   enddo
+ !$OMP END PARALLEL DO
 
 end
