5385064cd87ac0d0c4587d8764d07a84b3eab5cb
Emmanuel Giner <giner.emmanuel@gmail.com>
2021-10-07 18:25:09 +0200

    added test for sym

--- stable/mu_of_r_ints_bis/test_ints.irp.f
+++ stable/mu_of_r_ints_bis/test_ints.irp.f
@@ -2,6 +2,8 @@ program test_ints
  implicit none
  call test_ao
  call test_mo
+ call test_mo_sym
+ call test_ao_sym
 
 end
 
@@ -17,13 +19,13 @@ subroutine test_mo
      map_int =  get_mo_two_e_int_erf_mu_of_r(i,j,k,l,mo_int_erf_mu_of_r_map)
      prov_int = mo_mu_of_r_two_ints_phys(i,j,k,l)
      contrib = dabs(map_int  - prov_int)
-     print*,map_int,prov_int,contrib
+!     print*,map_int,prov_int,contrib
      accu += contrib
     enddo
    enddo
   enddo
  enddo
- print*,'accu = ',accu/dble(mo_num**4)
+ print*,'accu MO = ',accu/dble(mo_num**4)
 
 end
 
@@ -39,12 +41,52 @@ subroutine test_ao
      map_int =  get_ao_two_e_int_erf_mu_of_r(i,j,k,l,ao_int_erf_mu_of_r_map)
      prov_int = ao_mu_of_r_two_ints_phys(i,j,k,l)
      contrib = dabs(map_int  - prov_int)
-     print*,map_int,prov_int,contrib
+!     print*,map_int,prov_int,contrib
+     accu += contrib
+    enddo
+   enddo
+  enddo
+ enddo
+ print*,'accu AO = ',accu/dble(ao_num**4)
+
+end
+
+subroutine test_mo_sym
+ implicit none
+ integer :: i,j,k,l
+ double precision :: get_mo_two_e_int_erf_mu_of_r,int_direct,int_sym,accu,contrib
+ accu = 0.d0
+ do i = 1, mo_num
+  do j = 1, mo_num
+   do k = 1, mo_num
+    do l = 1, mo_num
+     int_direct =  get_mo_two_e_int_erf_mu_of_r(i,j,k,l,mo_int_erf_mu_of_r_map)
+     int_sym    =  get_mo_two_e_int_erf_mu_of_r(j,i,l,k,mo_int_erf_mu_of_r_map)
+     contrib = dabs(int_direct - int_sym)
+     accu += contrib
+    enddo
+   enddo
+  enddo
+ enddo
+ print*,'accu = ',accu/dble(mo_num**4)
+end
+
+subroutine test_ao_sym
+ implicit none
+ integer :: i,j,k,l
+ double precision :: get_ao_two_e_int_erf_mu_of_r,int_direct,int_sym,accu,contrib
+ accu = 0.d0
+ do i = 1, ao_num
+  do j = 1, ao_num
+   do k = 1, ao_num
+    do l = 1, ao_num
+     int_direct =  get_ao_two_e_int_erf_mu_of_r(i,j,k,l,ao_int_erf_mu_of_r_map)
+     int_sym    =  get_ao_two_e_int_erf_mu_of_r(j,i,l,k,ao_int_erf_mu_of_r_map)
+     contrib = dabs(int_direct - int_sym)
      accu += contrib
     enddo
    enddo
   enddo
  enddo
  print*,'accu = ',accu/dble(ao_num**4)
-
 end
