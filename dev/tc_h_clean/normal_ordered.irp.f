deabd047a33464e756fd5a96420d0633e4e0b2d8
Emmanuel Giner <giner.emmanuel@gmail.com>
2021-11-03 23:11:21 +0100

    normal order OK in the Slater rules

--- dev/tc_h_clean/normal_ordered.irp.f
+++ dev/tc_h_clean/normal_ordered.irp.f
@@ -1,11 +1,11 @@
 BEGIN_PROVIDER [ double precision, normal_two_body, (mo_num, mo_num, mo_num, mo_num)]
  implicit none
  use bitmasks ! you need to include the bitmasks_module.f90 features
- integer :: i,j,h1,p1,h2,p2
- integer :: kkk,k,m,l,n,hh1,hh2,pp1,pp2,kk
+ integer :: i,h1,p1,h2,p2
+ integer :: hh1,hh2,pp1,pp2
  integer                        :: occ(N_int*bit_kind_size,2),Ne(2)
  integer(bit_kind), allocatable :: key_i_core(:,:)
- double precision :: integral
+ double precision :: hthree_aba,hthree_aaa,hthree_aab
  allocate(key_i_core(N_int,2))
  if(core_tc_op)then
   do i = 1, N_int
@@ -19,65 +19,37 @@ BEGIN_PROVIDER [ double precision, normal_two_body, (mo_num, mo_num, mo_num, mo_
  normal_two_body = 0.d0
  do hh1 = 1, n_act_orb
   h1 = list_act(hh1) 
+  do pp1 = hh1, n_act_orb
+   p1 = list_act(pp1)
+   do hh2 = 1, n_act_orb
+    h2 = list_act(hh2) 
+    do pp2 = hh2, n_act_orb
+     p2 = list_act(pp2)
+     ! opposite spin double excitations 
+     call give_aba_contraction(h1,h2,p1,p2,Ne,occ,hthree_aba)
+     ! same spin double excitations with opposite spin contributions 
+     call give_aab_contraction(h1,h2,p1,p2,Ne,occ,hthree_aab)
+     ! same spin double excitations with same spin contributions 
+     if(Ne(2).ge.3)then
+      call give_aaa_contraction(h1,h2,p1,p2,Ne,occ,hthree_aaa)
+     else
+      hthree_aaa = 0.d0
+     endif
+     normal_two_body(p2,h2,p1,h1) = 0.5d0*(hthree_aba + hthree_aab + hthree_aaa)
+    enddo
+   enddo
+  enddo
+ enddo
+
+ do hh1 = 1, n_act_orb
+  h1 = list_act(hh1) 
   do pp1 = hh1+1, n_act_orb
    p1 = list_act(pp1)
    do hh2 = 1, n_act_orb
     h2 = list_act(hh2) 
     do pp2 = hh2+1, n_act_orb
      p2 = list_act(pp2)
-      do kkk = 1, Ne(2) 
-       ! double alpha / alpha 
-       kk = occ(kkk,2)
-       k = kk
-       j = h1
-       m = h2
-       l = p1
-       n = p2
-       ! direct beta - alpha/alpha
-       call give_integrals_3_body(j,m,k,l,n,k,integral)
-       normal_two_body(p2,h2,p1,h1) += -2.d0 * integral 
-       ! three_body_5_index_132
-       call give_integrals_3_body(j,m,k,k,l,n,integral)
-       normal_two_body(p2,h2,p1,h1) += -integral 
-       ! three_body_5_index_312
-       call give_integrals_3_body(j,m,k,n,k,l,integral)
-       normal_two_body(p2,h2,p1,h1) += -integral 
-       ! three_body_5_index_exch_12
-       call give_integrals_3_body(j,m,k,n,l,k,integral)
-       normal_two_body(p2,h2,p1,h1) += integral 
-       ! three_body_5_index_exch_13 
-       call give_integrals_3_body(j,m,k,k,l,n,integral)
-       normal_two_body(p2,h2,p1,h1) += integral 
-       ! three_body_5_index_exch_32
-       call give_integrals_3_body(j,m,k,k,n,l,integral)
-       normal_two_body(p2,h2,p1,h1) += integral 
-      enddo
-      do kkk = Ne(2)+1, Ne(1) ! alpha/alpha/alpha
-       kk = occ(kkk,1)
-       k = kk
-       j = h1
-       m = h2
-       l = p1
-       n = p2
-       ! direct 
-       call give_integrals_3_body(j,m,k,l,n,k,integral)
-       normal_two_body(p2,h2,p1,h1) += -2.d0 * integral 
-       ! three_body_5_index_132
-       call give_integrals_3_body(j,m,k,k,l,n,integral)
-       normal_two_body(p2,h2,p1,h1) += -integral 
-       ! three_body_5_index_312
-       call give_integrals_3_body(j,m,k,n,k,l,integral)
-       normal_two_body(p2,h2,p1,h1) += -integral 
-       ! three_body_5_index_exch_12
-       call give_integrals_3_body(j,m,k,n,l,k,integral)
-       normal_two_body(p2,h2,p1,h1) += integral 
-       ! three_body_5_index_exch_13 
-       call give_integrals_3_body(j,m,k,k,l,n,integral)
-       normal_two_body(p2,h2,p1,h1) += integral 
-       ! three_body_5_index_exch_32
-       call give_integrals_3_body(j,m,k,k,n,l,integral)
-       normal_two_body(p2,h2,p1,h1) += integral 
-      enddo 
+     normal_two_body(h2,p2,h1,p1) = normal_two_body(p2,h2,p1,h1)
     enddo
    enddo
   enddo
