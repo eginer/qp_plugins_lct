8989d8ead9cc6f43854f018e2735f40578614eb8
Diata Traore <dtraore@master.lct.jussieu.fr>
2023-03-22 19:05:00 +0100

    adding s2 to the lr output

--- dev/linear_response/linear_response_K_equal_0.irp.f
+++ dev/linear_response/linear_response_K_equal_0.irp.f
@@ -35,7 +35,7 @@ subroutine diagonalize_lr_k_equal_0
  double precision :: cpu_time_1, cpu_time_2
  double precision :: omp_time_1, omp_time_2
  integer, allocatable ::  ipiv(:)
- integer :: n
+ integer :: n,istate
  
  !###############################################################
  print*, "Allocate matrices"
@@ -108,21 +108,20 @@ subroutine diagonalize_lr_k_equal_0
  call cpu_time (cpu_time_1)
  omp_time_1 = omp_get_wtime()
 
-  print*,"AB matrix"
-  do det_I = 1, dim_half
-   write(*,'(100(f10.5,x))'), AB_matrix(1:dim_half, det_I)
-  enddo
-  print*, "S matrix"
-  do det_I = 1, dim_half
-   write(*,'(100(f10.5,x))'), S_matrix(1:dim_half, det_I)
-  enddo
+! print*,"AB matrix"
+! do det_I = 1, dim_half
+!  write(*,'(100(f10.5,x))'), AB_matrix(1:dim_half, det_I)
+! enddo
+! print*, "S matrix"
+! do det_I = 1, dim_half
+!  write(*,'(100(f10.5,x))'), S_matrix(1:dim_half, det_I)
+! enddo
 
   mat_a = AB_matrix
   mat_b = S_matrix
   call dggev("N", "V", dim, mat_a, dim, mat_b, dim, alphar, alphai, beta, &
   & eigenvectors_left,dim,eigenvectors,dim, work, lwork, info)
  
- deallocate(eigenvectors_left, eigenvectors)
  deallocate(mat_a, mat_b, AB_matrix, S_matrix)
 
  eigenvalues = alphar/beta
@@ -131,13 +130,82 @@ subroutine diagonalize_lr_k_equal_0
  print*, "END Eigenvalue problem - CPUTIME =", cpu_time_2 - cpu_time_1
  print*, "END Eigenvalue problem - WALLCLOCK TIME =", omp_time_2 - omp_time_1
 
-call dlasrt("I", dim, eigenvalues,info)
+ integer, allocatable :: iorder(:)
+ allocate(iorder(dim))
+ double precision, allocatable :: eigenvectors_sort(:,:), eigenvectors_transp(:,:)
+ allocate(eigenvectors_sort(dim, dim))
+ allocate(eigenvectors_transp(dim_half, dim_half))
+ do i = 1, dim
+   iorder(i) = i
+ enddo
+ call dsort(eigenvalues,iorder,dim) ! sort eigenvalues : dim_half of negatives , dim_half of positives in mirror
+!call dlasrt("I", dim, eigenvalues,info)
  print*, "Eigenvalues (alphareal/beta)"
  do det_I=1, dim
   print*, eigenvalues(det_I)
  enddo
+ do i = 1, dim
+  do j = 1, dim
+   ! the first dim_half are of negative eigenvalues, the second dim_half are the one interesting 
+   eigenvectors_sort(j,i) = eigenvectors(j,iorder(i)) ! sort the corresponding eigenvectors 
+  enddo
+ enddo
+!print*,'eigenvectors '
+!do i = 1, dim
+! write(*,'(I2,X,100(F8.5,X))')i,eigenvectors_sort(i,1:dim)
+!enddo
+  do i = 1, dim_half
+   do j = 1, dim_half
+    ! eigenvectors(i,j) = <det_i|Psi_j>
+    ! eigenvectors_transp(j,i) = <Psi_j| det_i> 
+    eigenvectors_transp(j,i) = eigenvectors_sort(i,j+dim_half) ! we obtain the interesting one
+   enddo
+  enddo
+!print*,'eigenvectors transp'
+!do i = 1, dim_half
+! write(*,'(I2,X,100(F8.5,X))')i,eigenvectors_transp(i,1:dim_half)
+!enddo
+ 
+  !! Computation of S2
+  double precision, allocatable :: S2_values_lr(:), norm_vec(:)
+  double precision :: s2_ij,s2_ii
+  allocate(S2_values_lr(dim_half), norm_vec(dim_half))
+  S2_values_lr = 0.d0
+  norm_vec = 0.d0
+  i=0
+  do det_I = 1, N_det
+   if(det_I == hf_index) cycle
+   i+= 1
+!  call get_s2(psi_det(1,1,det_I),psi_det(1,1,det_I),N_int,s2_ii)
+   do istate = 1, dim_half
+!   S2_values_lr(istate) += s2_ii * eigenvectors_transp(istate,i) * eigenvectors_transp(istate,i)
+    norm_vec(istate) += eigenvectors_transp(istate,i) * eigenvectors_transp(istate,i)
+   enddo
+   j = 0
+   do det_J = 1, N_det
+    if(det_J == hf_index) cycle
+     j += 1
+     call get_s2(psi_det(1,1,det_I),psi_det(1,1,det_J),N_int,s2_ij)
+     ! s2 = <det_i| S^2 | det_j>
+     do istate = 1, dim_half
+      S2_values_lr(istate) += 1.d0 * s2_ij * eigenvectors_transp(istate,i) * eigenvectors_transp(istate,j)
+     enddo
+   enddo
+  enddo
+  do i = 1, dim_half
+   S2_values_lr(i) *= 1.d0/(norm_vec(i))
+  enddo
+ !do istate = 1, dim_half
+ ! print*,'S2_values_lr',istate,S2_values_lr(istate)
+ !enddo
+ print*, " Half eigenvalues "
+ do istate=1, dim_half
+  print*, eigenvalues(istate+dim_half), S2_values_lr(istate)
+ enddo
+
 
  deallocate(work, alphar, alphai, beta)
+ deallocate(eigenvectors_left, eigenvectors)
 end subroutine
 
 
