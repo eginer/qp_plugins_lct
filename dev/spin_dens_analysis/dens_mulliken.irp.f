47e4488d606532e8929a90ea4be733466f399329
eginer <giner.emmanuel@gmail.com>
2023-11-16 15:55:40 +0100

    made density mulliken analysis for all states

--- dev/spin_dens_analysis/dens_mulliken.irp.f
+++ dev/spin_dens_analysis/dens_mulliken.irp.f
@@ -8,21 +8,18 @@ end
 
 subroutine routine
  implicit none
- double precision :: accu
- integer :: i
- integer :: j
- print*,'Mulliken density analysis '
- accu= 0.d0
- do i = 1, nucl_num
-  print*,i,nucl_charge(i)-mulliken_density_densities(i),nucl_coord(i,3)
-  accu += nucl_charge(i)-mulliken_density_densities(i)
+ double precision :: accu_tot, accu_charge
+ integer :: i,istate
+ do istate = 1, N_states
+  print*,'Mulliken density analysis for state ',istate
+  accu_charge = 0.d0
+  accu_tot = 0.d0
+  do i = 1, nucl_num
+   print*,i,nucl_charge(i),nucl_charge(i)-mulliken_density_densities(i,istate),nucl_coord(i,3)
+   accu_charge += nucl_charge(i)-mulliken_density_densities(i,istate)
+   accu_tot+= mulliken_density_densities(i,istate)
+  enddo
+  print*,'net charge = ',accu_charge
+  print*,'total elec = ',accu_tot
  enddo
- double precision, allocatable :: mull_dens_atoms(:)
- allocate(mull_dens_atoms(nucl_num))
-
-! call density_mulliken_density_mat(one_e_dm_ao, mull_dens_atoms) 
-! print*,'test '
-! do i = 1, nucl_num
-!  print*,i,mulliken_density_densities(i),mull_dens_atoms(i), dabs(mulliken_density_densities(i)-mull_dens_atoms(i))
-! enddo
 end
