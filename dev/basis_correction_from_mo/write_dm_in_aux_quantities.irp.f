2c9b665465c128c537581623693f09e816a8ef9a
Diata Traore <dtraore@master.lct.jussieu.fr>
2024-01-18 11:22:25 +0100

    add write dm

--- dev/basis_correction_from_mo/write_dm_in_aux_quantities.irp.f
+++ dev/basis_correction_from_mo/write_dm_in_aux_quantities.irp.f
@@ -6,13 +6,22 @@ program write_dm_in_aux_quantities
 ! Input files are labeled mo_alpha_1rdm.txt and mo_beta_1rdm.txt
   END_DOC
 
+  read_wf=.True.
+  touch read_wf 
+  call build_dm_for_aux_quantities
+
+  end 
+
+  subroutine build_dm_for_aux_quantities
+  implicit none
+  ! Faire plutôt des allocates
   double precision, dimension (mo_num, mo_num) :: one_e_dm_beta_mo_from_file_mo_num_size
   double precision, dimension (mo_num, mo_num) :: one_e_dm_alpha_mo_from_file_mo_num_size
   double precision, dimension (n_act_orb, n_act_orb) :: one_e_dm_beta_mo_from_file
   double precision, dimension (n_act_orb, n_act_orb) :: one_e_dm_alpha_mo_from_file
   double precision, dimension (ao_num, ao_num) :: one_e_dm_alpha_ao_from_file
   double precision, dimension (ao_num, ao_num) :: one_e_dm_beta_ao_from_file
-  integer :: i, j
+  integer :: i, j, ii, jj
 
 
   one_e_dm_alpha_mo_from_file_mo_num_size = 0.d0 
@@ -26,20 +35,29 @@ program write_dm_in_aux_quantities
   ! READ 1-e alpha density matrix in MO basis:
   open(12, file="mo_alpha_1rdm.txt") 
   read(12,*) one_e_dm_alpha_mo_from_file
-  one_e_dm_alpha_mo_from_file = transpose(one_e_dm_alpha_mo_from_file)
 
   ! READ 1-e beta density matrix in MO basis:
   open(13, file="mo_beta_1rdm.txt") 
   read(13,*) one_e_dm_beta_mo_from_file
-  one_e_dm_beta_mo_from_file = transpose(one_e_dm_beta_mo_from_file)
 
-  do i=1, n_act_orb
-   do j=1, n_act_orb
-    one_e_dm_alpha_mo_from_file_mo_num_size(i+n_core_orb,j+n_core_orb) = one_e_dm_alpha_mo_from_file(i,j)
-    one_e_dm_beta_mo_from_file_mo_num_size(i+n_core_orb,j+n_core_orb) = one_e_dm_beta_mo_from_file(i,j) 
+  do ii=1, n_act_orb
+   i=list_act(ii)
+   do jj=1, n_act_orb  
+    j=list_act(jj)
+    one_e_dm_alpha_mo_from_file_mo_num_size(i,j) = one_e_dm_alpha_mo_from_file(ii,jj)
+    one_e_dm_beta_mo_from_file_mo_num_size(i,j) = one_e_dm_beta_mo_from_file(ii,jj) 
    enddo
   enddo
-  
+   
+  do i=1, mo_num
+   do j=1, mo_num
+    print*, 'i=', i, 'j=', j
+    print*, one_e_dm_alpha_mo_from_file_mo_num_size(i,j) - one_e_dm_mo_alpha(i,j,1), one_e_dm_mo_alpha(i,j,1) 
+    print*, one_e_dm_beta_mo_from_file_mo_num_size(i,j) - one_e_dm_mo_beta(i,j,1), one_e_dm_mo_beta(i,j,1) 
+    print*, '-------------------------------------------'
+   enddo
+  enddo
+
   ! BUILD density matrix in AO basis:
   call mo_to_ao_no_overlap(one_e_dm_alpha_mo_from_file_mo_num_size,mo_num,one_e_dm_alpha_ao_from_file,ao_num)
   call mo_to_ao_no_overlap(one_e_dm_beta_mo_from_file_mo_num_size,mo_num,one_e_dm_beta_ao_from_file,ao_num)
